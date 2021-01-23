c648a517e3f97b7eb93ed33f7aba5e598bbcd383
Hector Martin <marcan@marcan.st>
2021-01-23 22:35:07 +0900

    memdump.py: add simple memory dumper
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ proxyclient/memdump.py
@@ -0,0 +1,19 @@
+
+import sys
+from setup import *
+
+p = 0x800000000
+limit = u.base
+block = 0x40000
+
+while p < limit:
+    f = "mem/0x%x.bin" % p
+    if os.path.exists(f):
+        p += block
+        continue
+
+    print("dumping 0x%x..." % p)
+
+    data = iface.readmem(p, block)
+    open(f, "wb").write(data)
+    p += block
