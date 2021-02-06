ec59b0d2c715c6f8f3e7610c424388b81d59abf0
Hector Martin <marcan@marcan.st>
2021-02-06 16:34:35 +0900

    chainload: do proper Mach-O parsing
    This is required for payloads to work properly
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/chainload.py
+++ proxyclient/chainload.py
@@ -1,28 +1,55 @@
 #!/usr/bin/env python3
 
 from setup import *
+from tgtypes import *
 
 payload = open(sys.argv[1], "rb").read()
 
-try:
-    # Try to use the m1n1 heap to avoid wasting 128MB RAM on every load
-    new_base = p.memalign(0x10000, len(payload))
-except:
-    # Fall back to proxy heap, which will be at the right place in old versions
-    new_base = u.memalign(0x10000, len(payload))
+obj = MachO.parse(payload)
+
+vmin, vmax = (1 << 64), 0
+
+entry = None
+
+for cmd in obj.cmds:
+    if cmd.cmd == LoadCmdType.SEGMENT_64:
+        vmin = min(vmin, cmd.args.vmaddr)
+        vmax = max(vmax, cmd.args.vmaddr + cmd.args.vmsize)
+    elif cmd.cmd == LoadCmdType.UNIXTHREAD:
+        entry = cmd.args[0].data.pc
+
+memory_size = vmax - vmin
 
 # FIXME: this will currently still waste the whole m1n1 size including payload area (64+MB) on each
 # chainload. The best way to fix this is to support in-place chainloading, which has other
 # advantages.
 
-print("Loading %d bytes to 0x%x" % (len(payload), new_base))
+try:
+    # Try to use the m1n1 heap to avoid wasting 128MB RAM on every load
+    new_base = p.memalign(0x10000, memory_size)
+except:
+    # Fall back to proxy heap, which will be at the right place in old versions
+    new_base = u.memalign(0x10000, memory_size)
 
-iface.writemem(new_base + 0x4000, payload[0x4000:], True)
+for cmd in obj.cmds:
+    if cmd.cmd == LoadCmdType.SEGMENT_64:
+        if cmd.args.fileoff == 0:
+            continue # do not load mach-o headers, not needed for now
+        dest = cmd.args.vmaddr - vmin + new_base
+        end = min(len(payload), cmd.args.fileoff + cmd.args.filesize)
+        size = end - cmd.args.fileoff
+        print("Loading %d bytes from 0x%x to 0x%x" % (size, cmd.args.fileoff, dest))
+        iface.writemem(dest, payload[cmd.args.fileoff:end], True)
+        if cmd.args.vmsize > size:
+            clearsize = cmd.args.vmsize - size
+            print("Zeroing %d bytes from 0x%x to 0x%x" % (clearsize, dest + size, dest + size + clearsize))
+            p.memset8(dest + size, 0, clearsize)
 
-entry = new_base + 0x4800
+p.dc_cvau(new_base, memory_size)
+p.ic_ivau(new_base, memory_size)
 
-p.dc_cvau(new_base, len(payload))
-p.ic_ivau(new_base, len(payload))
+entry -= vmin
+entry += new_base
 
 print("Jumping to 0x%x" % entry)
 
--- proxyclient/tgtypes.py
+++ proxyclient/tgtypes.py
@@ -2,7 +2,7 @@
 from construct import *
 
 BootArgs = Struct(
-    "revision"              / Hex(Int16ul),   
+    "revision"              / Hex(Int16ul),
     "version"               / Hex(Int16ul),
     Padding(4),
     "virt_base"             / Hex(Int64ul),
@@ -24,5 +24,92 @@ BootArgs = Struct(
     "cmdline"               / PaddedString(608, "ascii"),
     Padding(4),
     "boot_flags"            / Hex(Int64ul),
-    "mem_size_actual"       / Hex(Int64ul), 
+    "mem_size_actual"       / Hex(Int64ul),
+)
+
+LoadCmdType = "LoadCmdType" / Enum(Int32ul,
+    UNIXTHREAD = 0x05,
+    SEGMENT_64 = 0x19,
+    UUID = 0x1b,
+    BUILD_VERSION = 0x32,
+    DYLD_CHAINED_FIXUPS = 0x80000034,
+    FILESET_ENTRY = 0x80000035,
+)
+
+ArmThreadStateFlavor = "ThreadStateFlavor" / Enum(Int32ul,
+    THREAD64 = 6,
+)
+
+MachHeader = Struct(
+    "magic" / Hex(Int32ul),
+    "cputype" / Hex(Int32ul),
+    "cpusubtype" / Hex(Int32ul),
+    "filetype" / Hex(Int32ul),
+    "ncmds" / Hex(Int32ul),
+    "sizeofcmds" / Hex(Int32ul),
+    "flags" / Hex(Int32ul),
+    "reserved" / Hex(Int32ul),
+)
+
+VmProt = FlagsEnum(Int32sl,
+    PROT_READ = 0x01,
+    PROT_WRITE = 0x02,
+    PROT_EXECUTE = 0x04,
+)
+
+CmdUnixThread = GreedyRange(Struct(
+    "flavor" / ArmThreadStateFlavor,
+    "data" / Prefixed(ExprAdapter(Int32ul, obj_ * 4, obj_ / 4), Switch(this.flavor, {
+        ArmThreadStateFlavor.THREAD64: Struct(
+            "x" / Array(29, Hex(Int64ul)),
+            "fp" / Hex(Int64ul),
+            "lr" / Hex(Int64ul),
+            "sp" / Hex(Int64ul),
+            "pc" / Hex(Int64ul),
+            "cpsr" / Hex(Int32ul),
+            "flags" / Hex(Int32ul),
+        )
+    })),
+))
+
+
+CmdSegment64 = Struct(
+    "segname" / PaddedString(16, "ascii"),
+    "vmaddr" / Hex(Int64ul),
+    "vmsize" / Hex(Int64ul),
+    "fileoff" / Hex(Int64ul),
+    "filesize" / Hex(Int64ul),
+    "maxprot" / VmProt,
+    "initprot" / VmProt,
+    "nsects" / Int32ul,
+    "flags" / Hex(Int32ul),
+    "sections" / GreedyRange(Struct(
+        "sectname" / PaddedString(16, "ascii"),
+        "segname" / PaddedString(16, "ascii"),
+        "addr" / Hex(Int64ul),
+        "size" / Hex(Int64ul),
+        "offset" / Hex(Int32ul),
+        "align" / Hex(Int32ul),
+        "reloff" / Hex(Int32ul),
+        "nreloc" / Hex(Int32ul),
+        "flags" / Hex(Int32ul),
+        "reserved1" / Hex(Int32ul),
+        "reserved2" / Hex(Int32ul),
+        "reserved3" / Hex(Int32ul),
+    )),
+)
+
+Cmd = Struct(
+    "cmd" / Hex(LoadCmdType),
+    "args" / Prefixed(ExprAdapter(Int32ul, obj_ - 8, obj_ + 8), Switch(this.cmd, {
+        LoadCmdType.UNIXTHREAD: CmdUnixThread,
+        LoadCmdType.SEGMENT_64: CmdSegment64,
+        LoadCmdType.UUID: Hex(Bytes(16)),
+    }, default=GreedyBytes)),
+)
+
+MachO = Struct(
+    "header" / MachHeader,
+    "cmds" / Array(this.header.ncmds, Cmd),
+    "extradata" / GreedyBytes,
 )
