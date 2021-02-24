a0960fca815cf0e953be6e05e77ba16538037798
Hector Martin <marcan@marcan.st>
2021-02-24 21:45:56 +0900

    find_all_regs.py: test writability
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/find_all_regs.py
+++ proxyclient/find_all_regs.py
@@ -45,6 +45,12 @@ for op1 in range(1 << 3):
                 if v != BAD:
                     print("s3_%d_c%d_c%d_%d (3, %d, %d, %d, %d) = 0x%x" % (
                         op1, CRn, CRm, op2, op1, CRn, CRm, op2, v))
+                    try:
+                        u.msr((3, op1, CRn, CRm, op2), v)
+                    except:
+                        pass
+                    else:
+                        print(" - writable")
                 i += 1
 
 p.set_exc_guard(p.GUARD_OFF)
