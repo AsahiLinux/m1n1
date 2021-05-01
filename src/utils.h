68156fd8069b0d9b4edd11e9932f9e1cf3a87105
Hector Martin <marcan@marcan.st>
2021-05-01 19:02:49 +0900

    utils.h: Add alignment and field macros
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- src/utils.h
+++ src/utils.h
@@ -7,7 +7,15 @@
 
 #define printf(...) debug_printf(__VA_ARGS__)
 
-#define BIT(x) (1L << (x))
+#define BIT(x)                 (1L << (x))
+#define MASK(x)                (BIT(x) - 1)
+#define GENMASK(msb, lsb)      ((BIT((msb + 1) - (lsb)) - 1) << (lsb))
+#define _FIELD_LSB(field)      ((field) & ~(field - 1))
+#define FIELD_PREP(field, val) ((val) * (_FIELD_LSB(field)))
+#define FIELD_GET(field, val)  (((val) & (field)) / _FIELD_LSB(field))
+
+#define ALIGN_UP(x, a)   (((x) + ((a)-1)) & ~((a)-1))
+#define ALIGN_DOWN(x, a) ((x) & ~((a)-1))
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #define max(a, b) (((a) > (b)) ? (a) : (b))
