30e14f1a0b109da724c8c2167334cff79dca402c
Hector Martin <marcan@marcan.st>
2021-05-22 01:02:35 +0900

    find_regs: Modular version of find_all_regs.py
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ proxyclient/find_regs.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+import asm, struct
+import sysreg
+from proxyutils import GuardedHeap
+
+def _all():
+    for op1 in range(1 << 3):
+        for CRn in (0b1011, 0b1111):
+            for CRm in range(1 << 4):
+                for op2 in range(1 << 3):
+                    yield 3, op1, CRn, CRm, op2
+
+dynamic_regs = [
+    sysreg.CNTVCT_ALIAS_EL0,
+    sysreg.CNTPCT_ALIAS_EL0,
+]
+
+impdef_regs = list(_all())
+static_regs = [i for i in _all() if i not in dynamic_regs]
+
+def find_regs(u, regs=None, block=1024, call=None, values=True):
+    if regs is None:
+        regs = impdef_regs
+
+    p = u.proxy
+    iface = u.iface
+
+    data_len = 8 * block
+
+    with GuardedHeap(u.heap) as heap:
+        data_buffer = heap.malloc(data_len)
+
+        template = asm.ARMAsm("""
+            mov x2, x0
+            mrs x2, s3_0_c0_c0_0
+            str x2, [x1], #8
+        """, 0x1000)
+
+        mov, mrs, st = struct.unpack("3I", template.data)
+
+        data = []
+
+        BAD = 0xacce5515abad1dea
+        OOPS = 0xdeadc0dedeadc0de
+
+        rblock = []
+        iregs = iter(regs)
+
+        while True:
+            insns = []
+            bregs = []
+            for i in iregs:
+                op0, op1, CRn, CRm, op2 = enc = sysreg.sysreg_parse(i)
+                bregs.append(enc)
+                assert op0 == 3
+
+                insns.extend((mov, mrs | (op1 << 16) | (CRn << 12) | (CRm << 8) | (op2 << 5), st))
+
+                if len(bregs) >= block:
+                    break
+
+            if not bregs:
+                break
+
+            p.memset64(data_buffer, OOPS, data_len)
+            u.exec(insns, BAD, data_buffer, call=call, silent=True, ignore_exceptions=True)
+            data = iface.readmem(data_buffer, 8 * len(bregs))
+            for reg, val in zip(bregs, struct.unpack(f"<{len(bregs)}Q", data)):
+                if val == OOPS:
+                    raise Exception(f"Failed to execute reg-finder code at {reg}")
+                if val != BAD:
+                    if values:
+                        yield reg, val
+                    else:
+                        yield reg
+
+if __name__ == "__main__":
+    from setup import *
+
+    p.iodev_set_usage(IODEV.FB, 0)
+
+    for reg, val in find_regs(u):
+        print(f"{sysreg_name(reg)} ({', '.join(map(str, reg))}) = 0x{val:x}")
+
+        try:
+            u.msr(reg, v, silent=True)
+        except:
+            print(" - READONLY")
+        try:
+            u.mrs(reg, silent=True, call="el1")
+        except:
+            print(" - ### EL2 only ###")
+        try:
+            u.mrs(reg, silent=True, call="el0")
+        except:
+            pass
+        else:
+            print(" - *** EL0 accessible ***")
--- proxyclient/find_sprr_regs.py
+++ proxyclient/find_sprr_regs.py
@@ -1,92 +1,34 @@
 from setup import *
+from find_regs import find_regs
 import asm
 
 p.iodev_set_usage(IODEV.FB, 0)
 
-code_len = 12 * 16 * 8 + 4
-data_len = 8 * 16 * 8
-
-code_buffer = p.malloc(code_len)
-data_buffer = p.malloc(data_len)
-
-template = asm.ARMAsm("""
-    mov x2, x0
-    mrs x2, s3_0_c0_c0_0
-    str x2, [x1], #8
-    ret
-""", code_buffer)
-
-mov, mrs, st, ret = struct.unpack("4I", template.data)
-
-data = []
-
-BAD = 0xacce5515abad1dea
-
-
-def find_regs(call=None):
-    if not call:
-        call = p.call
-    regs = set()
-    for op1 in range(1 << 3):
-        for CRn in (0b1011, 0b1111):
-            mrs0 = mrs | (op1 << 16) | (CRn << 12)
-            insns = []
-            for CRm in range(1 << 4):
-                for op2 in range(1 << 3):
-                    insns.extend((mov, mrs0 | (CRm << 8) | (op2 << 5), st))
-            insns.append(ret)
-            iface.writemem(code_buffer, struct.pack("<385I", *insns))
-            p.dc_cvau(code_buffer, code_len)
-            p.ic_ivau(code_buffer, code_len)
-
-            p.set_exc_guard(GUARD.SILENT | GUARD.SKIP)
-            call(code_buffer, BAD, data_buffer)
-            cnt = p.get_exc_count()
-
-            data = iface.readmem(data_buffer, data_len)
-            d = struct.unpack("<128Q", data)
-            i = 0
-            for CRm in range(1 << 4):
-                for op2 in range(1 << 3):
-                    v = d[i]
-                    if v != BAD:
-                        regs.add((3, op1, CRn, CRm, op2))
-                    i += 1
-
-    return regs
-
-p.set_exc_guard(GUARD.SILENT | GUARD.SKIP)
 all_regs = set()
-with u.mmu_disabled():
-    for reg in [(3, 6, 15, 1, 0), (3, 6, 15, 1, 2)]: # , (3, 6, 15, 1, 4)]: 
+for reg in [SPRR_CONFIG_EL1, GXF_CONFIG_EL1]:
+    old_regs = find_regs(u)
+    u.msr(reg, 1)
+    new_regs = find_regs(u)
 
-        old_regs = find_regs()
-        u.msr(reg, 1)
-        new_regs = find_regs()
+    all_regs = all_regs.union(new_regs)
 
-        all_regs = all_regs.union(new_regs)
+    diff_regs = new_regs - old_regs
 
-        diff_regs = new_regs - old_regs
+    print(reg)
+    for r in sorted(diff_regs):
+        print("  %s --> %lx" % (list(r), u.mrs(r)))
 
-        print(reg)
-        for r in sorted(diff_regs):
-            print("  %s --> %lx" % (list(r), u.mrs(r)))
+u.msr(GXF_CONFIG_EL1, 0)
+u.msr(SPRR_CONFIG_EL1, 0)
 
-
-    #u.msr((3, 6, 15, 1, 4), 0)
-    u.msr((3, 6, 15, 1, 2), 0)
-    u.msr((3, 6, 15, 1, 0), 0)
-
-gxf_regs = find_regs(call=p.gl2_call)
+gxf_regs = find_regs(u, call="gl2")
 
 print("GL2")
 for r in sorted(gxf_regs - all_regs):
-    print("  %s -> %lx" % (list(r), u.mrs(r, call=p.gl2_call)))
+    print("  %s -> %lx" % (list(r), u.mrs(r, call="gl2")))
 
-
-gxf_regs = find_regs(call=p.gl1_call)
+gxf_regs = find_regs(u, call="gl1")
 
 print("GL1")
 for r in sorted(gxf_regs - all_regs):
-    print("  %s -> %lx" % (list(r), u.mrs(r, call=p.gl1_call)))
-
+    print("  %s -> %lx" % (list(r), u.mrs(r, call="gl1")))
