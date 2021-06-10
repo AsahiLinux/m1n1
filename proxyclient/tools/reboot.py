457bf0735f236a605924b548f1c9d5a29b8926f7
Janne Grunau <j@jannau.net>
2021-06-10 23:53:03 +0200

    proxy/tools: add reboot.py
    Signed-off-by: Janne Grunau <j@jannau.net>

---
+++ proxyclient/tools/reboot.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+import sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
+from m1n1.setup import *
+
+p.reboot()
