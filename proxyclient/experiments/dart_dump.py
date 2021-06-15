bb6ef8caa1d58f19c56ae89a66182085c8e82308
Hector Martin <marcan@marcan.st>
2021-06-16 02:31:30 +0900

    experiments/dart_dump.py: Port to RegMap
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/experiments/dart_dump.py
+++ proxyclient/experiments/dart_dump.py
@@ -8,8 +8,51 @@ import struct
 from m1n1.setup import *
 from m1n1 import asm
 
-class DART:
+class R_ERROR(Register32):
+    FLAG = 31
+    STREAM = 27, 24
+    CODE = 23, 0
+    READ_FAULT = 4
+    WRITE_FAULT = 3
+    NO_PTE = 2
+    NO_PMD = 1
+    NO_TTBR = 0
+
+class R_STREAM_COMMAND(Register32):
+    INVALIDATE = 20
+    BUSY = 2
+
+class R_TCR(Register32):
+    BYPASS_DAPF = 12
+    BYPASS_DART = 8
+    TRANSLATE_ENABLE = 7
+
+class R_TTBR(Register32):
+    VALID = 31
+    ADDR = 30, 0
+
+class R_REMAP(Register32):
+    MAP3 = 31, 24
+    MAP2 = 23, 16
+    MAP1 = 15, 8
+    MAP0 = 7, 0
+
+class DART(RegMap):
+    STREAM_COMMAND  = 0x20, R_STREAM_COMMAND
+    STREAM_SELECT   = 0x34, Register32
+    ERROR           = 0x40, R_ERROR
+    ERROR_ADDR_LO   = 0x50, Register32
+    ERROR_ADDR_HI   = 0x54, Register32
+    REMAP           = irange(0x80, 4, 4), R_REMAP
+
+    TCR             = irange(0x100, 16, 4), R_TCR
+    TTBR0           = irange(0x200, 16, 16), R_TTBR
+    TTBR1           = irange(0x204, 16, 16), R_TTBR
+    TTBR2           = irange(0x208, 16, 16), R_TTBR
+    TTBR3           = irange(0x20c, 16, 16), R_TTBR
+
     def __init__(self, base):
+        super().__init__(p, base)
         self.base = base
 
     def dump_table2(self, base, l1_addr):
@@ -36,30 +79,29 @@ class DART:
             self.dump_table2(base + i*0x2000000, pte & ~0b11)
 
     def dump_ttbr(self, idx, ttbr):
-        if not ttbr & (1<<31):
+        if not ttbr.VALID:
             return
 
-        l1_addr = (ttbr & ~(1<<31)) << 12
+        l1_addr = (ttbr.ADDR) << 12
         print("  TTBR%d: %09x" % (idx, l1_addr))
 
         self.dump_table(0, l1_addr)
 
 
     def dump_device(self, idx):
-        tcr = p.read32(self.base + 0x100 + 4*idx)
-        ttbrs = [p.read32(self.base + 0x200 + 16*idx + 4*i) for i in range(4)]
-        ttbrs_str = " ".join("%08x" % t for t in ttbrs)
-        print("dev %02x: TCR=%08x TTBRs = [%s]" % (idx, tcr, ttbrs_str))
+        tcr = self.TCR[idx].reg
+        ttbrs = self.TTBR0[idx], self.TTBR1[idx], self.TTBR2[idx], self.TTBR3[idx]
+        print(f"dev {idx:02x}: TCR={tcr!s} TTBRs = [{', '.join(map(str, ttbrs))}]")
 
-        if (tcr & 0x80) and (tcr & 0x100):
+        if tcr.TRANSLATE_ENABLE and tcr.BYPASS_DART:
             print("  mode: INVALID")
-        elif tcr & 0x80:
+        elif tcr.TRANSLATE_ENABLE:
             print("  mode: TRANSLATE")
 
             for idx, ttbr in enumerate(ttbrs):
-                self.dump_ttbr(idx, ttbr)
-        elif tcr & 0x100:
-            print("  mode: 0x100, UNKNOWN MODE")
+                self.dump_ttbr(idx, ttbr.reg)
+        elif tcr.BYPASS_DART:
+            print("  mode: BYPASS")
         else:
             print("  mode: UNKNOWN")
 
@@ -75,3 +117,4 @@ else:
 # note that there's another range just before this one
 disp0 = DART(dart_addr)
 disp0.dump_all()
+disp0.dump_regs()
