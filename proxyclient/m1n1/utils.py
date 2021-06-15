48b4466abf6de6e149b887cad54c8fee5105899c
Hector Martin <marcan@marcan.st>
2021-06-15 19:42:30 +0900

    m1n1.utils: Rewrite AddrLookup to improve performance & fix bugs
    * Fix correctness issues (ranges could end up overlapping)
    * Split ranges instead of merging them on overlap (previously a single
      range overlapping everything would reduce the lookup to a linear
      search over all ranges)
    * Support removal and generic mutations by splitting out a generic
      RangeMap class that can be used with arbitrary values/collections
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/m1n1/utils.py
+++ proxyclient/m1n1/utils.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
 from enum import Enum
-import bisect
+import bisect, copy
 from construct import Adapter, Int64ul, Int32ul, Int16ul, Int8ul
 
 __all__ = []
@@ -162,87 +162,229 @@ class RegAdapter(Adapter):
     def _encode(self, obj, context, path):
         return obj.value
 
-class AddrLookup:
+class RangeMap:
     def __init__(self):
-        self.__addr = []
+        self.__start = []
         self.__end = []
-        self.__ranges = []
+        self.__value = []
 
     def __len__(self):
-        return len(self.__addr)
+        return len(self.__start)
 
-    def __str__(self):
-        b = ""
-        for i in range(len(self)):
-            b += f"{i:4d}: {self.__addr[i]:#010x} - {self.__end[i]:#010x}"
-            if len(self.__ranges[i]) > 1:
-                b += f" {len(self.__ranges[i]):2d} sub ranges"
-            b += '\n'
-            for r, value in sorted(self.__ranges[i], key=lambda r: r[0].start):
-                b += f"      {r.start:#010x}   {len(r):#08x}   {value}: \n"
-        return b
+    def __contains(self, pos, addr):
+        if pos < 0 or pos >= len(self.__start):
+            return False
 
-    def __overlaps(self, zone, pos):
-        if self.__addr[pos] in zone or self.__end[pos] in zone:
-            return True
-        return self.__addr[pos] <= zone.start and zone.start < self.__end[pos]
+        return self.__start[pos] <= addr and addr <= self.__end[pos]
 
-    def __merge(self, a, b):
-        self.__addr.pop(b)
-        self.__end.pop(a)
-        self.__ranges[a] = sorted(self.__ranges[a] + self.__ranges[b], key=lambda r: len(r[0]))
-        self.__ranges.pop(b)
+    def __split(self, pos, addr):
+        self.__start.insert(pos + 1, addr)
+        self.__end.insert(pos, addr - 1)
+        self.__value.insert(pos + 1, copy.copy(self.__value[pos]))
 
-    def __append(self, pos, zone, value):
-        if zone.start < self.__addr[pos]:
-            self.__addr[pos] = zone.start
-        if zone.stop - 1 >  self.__end[pos]:
-            self.__end[pos] = zone.stop - 1
-        # insert sorted and merge identical ranges
-        sizes = [len(e[0]) for e in self.__ranges[pos]]
-        start = bisect.bisect_left(sizes, len(zone))
-        for subpos in range(start, len(self.__ranges[pos])):
-            e_zone, e_value = self.__ranges[pos][subpos]
-            if len(zone) < len(e_zone):
-                self.__ranges[pos].insert(subpos, (zone, [value]))
-                break
+    def lookup(self, addr, default=None):
+        addr = int(addr)
+
+        pos = bisect.bisect_left(self.__end, addr)
+        if self.__contains(pos, addr):
+            return self.__value[pos]
         else:
-            self.__ranges[pos].append((zone, [value]))
+            return default
+
+    def ranges(self):
+        return ((range(s, e + 1), v) for s, e, v in zip(self.__start, self.__end, self.__value))
+
+    def overlaps(self, zone):
+        if len(zone) == 0:
+            return
+
+        start = bisect.bisect_left(self.__end, zone.start)
+
+        for pos in range(start, len(self.__start)):
+            if self.__start[pos] >= zone.stop:
+                break
+            yield range(self.__start[pos], self.__end[pos] + 1), self.__value[pos]
+
+    def populate(self, zone, default=[]):
+        if len(zone) == 0:
+            return
+
+        start, stop = zone.start, zone.stop
+
+        # Starting insertion point, overlap inclusive
+        pos = bisect.bisect_left(self.__end, zone.start)
+
+        # Handle left-side overlap
+        if self.__contains(pos, zone.start) and self.__start[pos] != zone.start:
+            self.__split(pos, zone.start)
+            pos += 1
+            assert self.__start[pos] == zone.start
+
+        # Iterate through overlapping ranges
+        while start < stop:
+            if pos == len(self.__start):
+                # Append to end
+                val = copy.copy(default)
+                self.__start.append(start)
+                self.__end.append(stop - 1)
+                self.__value.append(val)
+                yield range(start, stop), val
+                break
+
+            assert self.__start[pos] >= start
+            if self.__start[pos] > start:
+                # Insert new range
+                boundary = stop
+                if pos < len(self.__start):
+                    boundary = min(stop, self.__start[pos])
+                val = copy.copy(default)
+                self.__start.insert(pos, start)
+                self.__end.insert(pos, boundary - 1)
+                self.__value.insert(pos, val)
+                yield range(start, boundary), val
+                start = boundary
+            else:
+                # Handle right-side overlap
+                if self.__end[pos] > stop - 1:
+                    self.__split(pos, stop)
+                # Add to existing range
+                yield range(self.__start[pos], self.__end[pos] + 1), self.__value[pos]
+                start = self.__end[pos] + 1
+
+            pos += 1
+        else:
+            assert start == stop
+
+    def compact(self, equal=lambda a, b: a == b, empty=lambda a: not a):
+        if len(self) == 0:
+            return
+
+        new_s, new_e, new_v = [], [], []
+
+        for pos in range(len(self)):
+            s, e, v = self.__start[pos], self.__end[pos], self.__value[pos]
+            if empty(v):
+                continue
+            if new_v and equal(last, v):
+                new_e[-1] = e
+            else:
+                new_s.append(s)
+                new_e.append(e)
+                new_v.append(v)
+                last = v
+
+        self.__start, self.__end, self.__value = new_s, new_e, new_v
+
+    def _assert(self, expect, val=lambda a:a):
+        state = []
+        for i, j, v in zip(self.__start, self.__end, self.__value):
+            state.append((i, j, val(v)))
+        if state != expect:
+            print(f"Expected: {expect}")
+            print(f"Got:      {state}")
+
+class AddrLookup(RangeMap):
+    def __str__(self):
+        b = [""]
+        for zone, values in self.ranges():
+            b.append(f"{zone.start:#11x} - {zone.stop - 1:#11x}")
+            if len(values) == 0:
+                b.append(f" (empty range)")
+            elif len(values) == 1:
+                b.append(f" : {values[0][0]}\n")
+            if len(values) > 1:
+                b.append(f" ({len(values):d} devices)\n")
+                for value, r in sorted(values, key=lambda r: r[1].start):
+                    b.append(f"      {r.start:#10x} - {r.stop - 1:#8x} : {value}\n")
+
+        return "".join(b)
 
     def add(self, zone, value):
-        pos = bisect.bisect(self.__addr, zone.start)
-        overlap_left = pos > 0 and self.__overlaps(zone, pos - 1)
-        overlap_right = pos < len(self) and self.__overlaps(zone, pos)
+        for r, values in self.populate(zone):
+            values.append((value, zone))
 
-        if overlap_left and overlap_right:
-            self.__merge(pos - 1, pos)
-            self.__append(pos - 1, zone, value)
-        elif overlap_left:
-            self.__append(pos - 1, zone, value)
-        elif overlap_right:
-            self.__append(pos, zone, value)
-        else:
-            self.__addr.insert(pos, zone.start)
-            self.__end.insert(pos, zone.stop - 1)
-            self.__ranges.insert(pos, [(zone, [value])])
+    def remove(self, zone, value):
+        for r, values in self.overlaps(zone):
+            try:
+                values.remove((value, zone))
+            except:
+                pass
 
     def lookup(self, addr, default='unknown'):
-        addr = int(addr)
-        pos = bisect.bisect(self.__addr, addr)
-        if pos == 0 or self.__end[pos - 1] < addr:
-            return (default, range(0, 1 << 64))
-
-        for r, value in self.__ranges[pos - 1]:
-            if addr in r:
-                return (value[0], r)
-        return (None, range(0, 0))
+        maps = super().lookup(addr)
+        return maps[0] if maps else (default, range(0, 1 << 64))
 
     def lookup_all(self, addr):
-        addr = int(addr)
-        pos = bisect.bisect(self.__addr, addr)
-        if pos == 0 or self.__end[pos - 1] < addr:
-            return []
-        return [(value, r) for r, value in self.__ranges[pos - 1] if addr in r]
+        return super().lookup(addr, [])
+
+    def _assert(self, expect, val=lambda a:a):
+        super()._assert(expect, lambda v: [i[0] for i in v])
 
 __all__.extend(k for k, v in globals().items()
                if (callable(v) or isinstance(v, type)) and v.__module__ == __name__)
+
+if __name__ == "__main__":
+    # AddrLookup test
+    a = AddrLookup()
+    a.add(range(0, 10), 0)
+    a._assert([
+        (0, 9, [0])
+    ])
+    a.add(range(10, 20), 1)
+    a._assert([
+        (0, 9, [0]), (10, 19, [1])
+    ])
+    a.add(range(20, 25), 2)
+    a._assert([
+        (0, 9, [0]), (10, 19, [1]), (20, 24, [2])
+    ])
+    a.add(range(30, 40), 3)
+    a._assert([
+        (0, 9, [0]), (10, 19, [1]), (20, 24, [2]), (30, 39, [3])
+    ])
+    a.add(range(0, 15), 4)
+    a._assert([
+        (0, 9, [0, 4]), (10, 14, [1, 4]), (15, 19, [1]), (20, 24, [2]), (30, 39, [3])
+    ])
+    a.add(range(0, 15), 5)
+    a._assert([
+        (0, 9, [0, 4, 5]), (10, 14, [1, 4, 5]), (15, 19, [1]), (20, 24, [2]), (30, 39, [3])
+    ])
+    a.add(range(21, 44), 6)
+    a._assert([
+        (0, 9, [0, 4, 5]), (10, 14, [1, 4, 5]), (15, 19, [1]), (20, 20, [2]), (21, 24, [2, 6]),
+        (25, 29, [6]), (30, 39, [3, 6]), (40, 43, [6])
+    ])
+    a.add(range(70, 80), 7)
+    a._assert([
+        (0, 9, [0, 4, 5]), (10, 14, [1, 4, 5]), (15, 19, [1]), (20, 20, [2]), (21, 24, [2, 6]),
+        (25, 29, [6]), (30, 39, [3, 6]), (40, 43, [6]), (70, 79, [7])
+    ])
+    a.add(range(0, 100), 8)
+    a._assert([
+        (0, 9, [0, 4, 5, 8]), (10, 14, [1, 4, 5, 8]), (15, 19, [1, 8]), (20, 20, [2, 8]),
+        (21, 24, [2, 6, 8]), (25, 29, [6, 8]), (30, 39, [3, 6, 8]), (40, 43, [6, 8]),
+        (44, 69, [8]), (70, 79, [7, 8]), (80, 99, [8])
+    ])
+    a.remove(range(21, 44), 6)
+    a._assert([
+        (0, 9, [0, 4, 5, 8]), (10, 14, [1, 4, 5, 8]), (15, 19, [1, 8]), (20, 20, [2, 8]),
+        (21, 24, [2, 8]), (25, 29, [8]), (30, 39, [3, 8]), (40, 43, [8]),
+        (44, 69, [8]), (70, 79, [7, 8]), (80, 99, [8])
+    ])
+    a.compact()
+    a._assert([
+        (0, 9, [0, 4, 5, 8]), (10, 14, [1, 4, 5, 8]), (15, 19, [1, 8]), (20, 24, [2, 8]),
+        (25, 29, [8]), (30, 39, [3, 8]), (40, 69, [8]), (70, 79, [7, 8]),
+        (80, 99, [8])
+    ])
+    a.remove(range(0, 100), 8)
+    a._assert([
+        (0, 9, [0, 4, 5]), (10, 14, [1, 4, 5]), (15, 19, [1]), (20, 24, [2]), (25, 29, []),
+        (30, 39, [3]), (40, 69, []), (70, 79, [7]), (80, 99, [])
+    ])
+    a.compact()
+    a._assert([
+        (0, 9, [0, 4, 5]), (10, 14, [1, 4, 5]), (15, 19, [1]), (20, 24, [2]), (30, 39, [3]),
+        (70, 79, [7])
+    ])
