8a0125ac1770fdadbbd9d2af584f94e2f3f87592
Hector Martin <marcan@marcan.st>
2021-06-21 02:05:08 +0900

    m1n1.trce.gpio: Use new register arrays
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/m1n1/trace/gpio.py
+++ proxyclient/m1n1/trace/gpio.py
@@ -11,20 +11,10 @@ class R_PIN(Register32):
     CONFIG      = 3, 1
     VALUE       = 0
 
-
 class GPIORegs(RegMap):
-    IRQ_ACT = 0x800
-    IRQ_GRP_SZ = 0x40
-
     PIN = irange(0x000, 212, 4), R_PIN
 
-    IRQ_GROUP0  = irange(IRQ_ACT + 0 * IRQ_GRP_SZ, (212 + 31) // 32, 4), Register32
-    IRQ_GROUP1  = irange(IRQ_ACT + 1 * IRQ_GRP_SZ, (212 + 31) // 32, 4), Register32
-    IRQ_GROUP2  = irange(IRQ_ACT + 2 * IRQ_GRP_SZ, (212 + 31) // 32, 4), Register32
-    IRQ_GROUP3  = irange(IRQ_ACT + 3 * IRQ_GRP_SZ, (212 + 31) // 32, 4), Register32
-    IRQ_GROUP4  = irange(IRQ_ACT + 4 * IRQ_GRP_SZ, (212 + 31) // 32, 4), Register32
-    IRQ_GROUP5  = irange(IRQ_ACT + 5 * IRQ_GRP_SZ, (212 + 31) // 32, 4), Register32
-    IRQ_GROUP6  = irange(IRQ_ACT + 6 * IRQ_GRP_SZ, (212 + 31) // 32, 4), Register32
+    IRQ_GROUP  = (irange(0x800, 7, 0x40), irange(0, (212 + 31) // 32, 4)), Register32
 
 def bits32(val, start):
     return [start + i for i in range(0, 32) if int(val) & (1 << i)]
@@ -66,42 +56,15 @@ class GPIOTracer(ADTDevTracer):
             return # ignore noisy SPI NOR CS
         self.log(f"{self.pn(index):14} W {val!s} ")
 
-    def r_IRQ_GROUP(self, val, index, grp):
+    def r_IRQ_GROUP(self, val, index):
+        (grp, index) = index
         if int(val) != 0:
             self.log(f"IRQ[{grp}] ACT {[self.pn(x) for x in bits32(val, index * 32)]}")
 
-    def r_IRQ_GROUP0(self, val, index):
-        self.r_IRQ_GROUP(val, index, 0)
-    def r_IRQ_GROUP1(self, val, index):
-        self.r_IRQ_GROUP(val, index, 1)
-    def r_IRQ_GROUP2(self, val, index):
-        self.r_IRQ_GROUP(val, index, 2)
-    def r_IRQ_GROUP3(self, val, index):
-        self.r_IRQ_GROUP(val, index, 3)
-    def r_IRQ_GROUP4(self, val, index):
-        self.r_IRQ_GROUP(val, index, 4)
-    def r_IRQ_GROUP5(self, val, index):
-        self.r_IRQ_GROUP(val, index, 5)
-    def r_IRQ_GROUP6(self, val, index):
-        self.r_IRQ_GROUP(val, index, 6)
-
-    def w_IRQ_GROUP(self, val, index, grp):
+    def w_IRQ_GROUP(self, val, index):
+        (grp, index) = index
         if int(val) == (1 << 32) - 1:
             self.log(f"IRQ[{grp}] ACK {index * 32} - {index * 32 + 31}")
         elif int(val) != 0:
             self.log(f"IRQ[{grp}] ACK {[self.pn(x) for x in bits32(val, index * 32)]}")
 
-    def w_IRQ_GROUP0(self, val, index):
-        self.w_IRQ_GROUP(val, index, 0)
-    def w_IRQ_GROUP1(self, val, index):
-        self.w_IRQ_GROUP(val, index, 1)
-    def w_IRQ_GROUP2(self, val, index):
-        self.w_IRQ_GROUP(val, index, 2)
-    def w_IRQ_GROUP3(self, val, index):
-        self.w_IRQ_GROUP(val, index, 3)
-    def w_IRQ_GROUP4(self, val, index):
-        self.w_IRQ_GROUP(val, index, 4)
-    def w_IRQ_GROUP5(self, val, index):
-        self.w_IRQ_GROUP(val, index, 5)
-    def w_IRQ_GROUP6(self, val, index):
-        self.w_IRQ_GROUP(val, index, 6)
