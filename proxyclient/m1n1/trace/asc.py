7632a57ff37351dee35e4d0702f31937a63a4e75
Hector Martin <marcan@marcan.st>
2021-06-21 02:05:23 +0900

    m1n1.trace.asc: Implement basic message handling/dispatch
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/m1n1/trace/asc.py
+++ proxyclient/m1n1/trace/asc.py
@@ -1,8 +1,13 @@
 # SPDX-License-Identifier: MIT
 
+from enum import Enum
 from ..hv import TraceMode
 from ..utils import *
 from . import ADTDevTracer
+
+class DIR(Enum):
+    RX = 0
+    TX = 1
 
 class R_OUTBOX_CTRL(Register32):
     EMPTY = 17
@@ -10,13 +15,55 @@ class R_OUTBOX_CTRL(Register32):
 class R_INBOX_CTRL(Register32):
     ENABLE = 1
 
+class R_MESSAGE(Register64):
+    TYPE    = 59, 52
+
+class R_INBOX1(Register64):
+    EP      = 7, 0
+
+class R_OUTBOX1(Register64):
+    OUTCNT  = 56, 52
+    INCNT   = 51, 48
+    OUTPTR  = 47, 44
+    INPTR   = 43, 40
+    EP      = 7, 0
+
 class ASCRegs(RegMap):
     INBOX_CTRL  = 0x8110, R_INBOX_CTRL
     OUTBOX_CTRL = 0x8114, R_OUTBOX_CTRL
-    INBOX0      = 0x8800, Register64
-    INBOX1      = 0x8808, Register64
-    OUTBOX0     = 0x8830, Register64
-    OUTBOX1     = 0x8838, Register64
+    INBOX0      = 0x8800, R_MESSAGE
+    INBOX1      = 0x8808, R_INBOX1
+    OUTBOX0     = 0x8830, R_MESSAGE
+    OUTBOX1     = 0x8838, R_OUTBOX1
+
+class MSG_EP_MAP(R_MESSAGE):
+    LAST    = 48
+    BASE    = 34, 32
+    BITMAP  = 31, 0
+
+class MSG_EP_ACK(R_MESSAGE):
+    LAST    = 48
+    EP      = 34, 32
+    FLAG    = 0
+
+class MSG_EP_START(R_MESSAGE):
+    EP      = 39, 32
+
+def msg(channel, message, direction=None, regtype=None, name=None):
+    def f(x):
+        x.is_message = True
+        x.direction = direction
+        x.channel = channel
+        x.message = message
+        x.regtype = regtype
+        x.name = name
+        return x
+    return f
+
+def msg_log(*args, **kwargs):
+    def x(self, r0, r1):
+        pass
+    return msg(*args, **kwargs)(x)
 
 class ASCTracer(ADTDevTracer):
     DEFAULT_MODE = TraceMode.SYNC
@@ -24,13 +71,78 @@ class ASCTracer(ADTDevTracer):
     REGMAPS = [ASCRegs, None]
     NAMES = ["asc", None]
 
+    EP_SYS = 0
+
     def w_OUTBOX_CTRL(self, val):
         self.log(f"OUTBOX_CTRL = {val!s}")
 
-    def w_INBOX1(self, val):
-        inbox0 = self.asc.cached.INBOX0.val
-        self.log(f"SEND: {inbox0:016x}:{val.value:016x}")
+    def w_INBOX1(self, inbox1):
+        inbox0 = self.asc.cached.INBOX0.reg
+        if self.verbose >= 2:
+            self.log(f"SEND: {inbox0.value:016x}:{inbox1.value:016x} " +
+                    f"{inbox0.str_fields()} | {inbox1.str_fields()}")
+        self.handle_msg(DIR.TX, inbox0, inbox1)
 
-    def r_OUTBOX1(self, val):
-        outbox0 = self.asc.cached.OUTBOX0.val
-        self.log(f"RECV: {outbox0:016x}:{val.value:016x}")
+    def r_OUTBOX1(self, outbox1):
+        outbox0 = self.asc.cached.OUTBOX0.reg
+        if self.verbose >= 2:
+            self.log(f"RECV: {outbox0.value:016x}:{outbox1.value:016x} " +
+                    f"{outbox0.str_fields()} | {outbox1.str_fields()}")
+        self.handle_msg(DIR.RX, outbox0, outbox1)
+
+    def init_state(self):
+        self.state.endpoints = set()
+        self.state.mbox = {}
+
+    def handle_msg(self, direction, r0, r1):
+        msgids = [
+            (direction, r1.EP, r0.TYPE),
+            (None, r1.EP, r0.TYPE),
+            (direction, r1.EP, None),
+            (None, r1.EP, None),
+            (direction, None, r0.TYPE),
+            (None, None, r0.TYPE),
+            (None, None, None),
+        ]
+        handler = None
+
+        for msgid in msgids:
+            handler, name, regtype = self.msgmap.get(msgid, (None, None, None))
+            if handler:
+                break
+        assert handler
+
+        if regtype is not None:
+            r0 = regtype(r0.value)
+
+        if handler.name is not None:
+            name = handler.name
+
+        d = ">" if direction == DIR.TX else "<"
+        self.log(f" {d}{r1.EP:02x}:{r0.TYPE:02x} {name} {r0.value:016x} ({r0.str_fields()})")
+        handler(r0, r1)
+
+    def start(self):
+        super().start()
+        self.msgmap = {}
+        for name, i in self.__class__.__dict__.items():
+            if not callable(i) or not getattr(i, "is_message", False):
+                continue
+            self.msgmap[i.direction, i.channel, i.message] = getattr(self, name), name, i.regtype
+
+    unknown =   msg_log(None, None, None, name="<unknown>")
+
+    INIT =      msg_log(EP_SYS, 6, DIR.TX)
+    HELLO =     msg_log(EP_SYS, 1, DIR.RX)
+    HELLO_ACK = msg_log(EP_SYS, 2, DIR.TX)
+
+    @msg(EP_SYS, 8, DIR.RX, MSG_EP_MAP)
+    def EP_MAP(self, r0, r1):
+        for i in range(32):
+            if r0.BITMAP & (1 << i):
+                ep = 32 * r0.BASE + i
+                self.log(f"  Registering endpoint #{ep:#02x}")
+                self.state.endpoints.add(ep)
+
+    EP_ACK =    msg_log(EP_SYS, 8, DIR.TX, MSG_EP_ACK)
+    EP_START =  msg_log(EP_SYS, 5, DIR.TX, MSG_EP_START)
