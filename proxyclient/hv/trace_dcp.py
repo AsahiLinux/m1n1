c490f74872bdf7a885c0f7e821868a54effbe373
Hector Martin <marcan@marcan.st>
2021-06-23 17:05:05 +0900

    m1n1.trace.asc: Move EP handlers to per-EP objects
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/hv/trace_dcp.py
+++ proxyclient/hv/trace_dcp.py
@@ -5,7 +5,7 @@ trace_device("/arm-io/dcp", True, ranges=[1])
 from m1n1.proxyutils import RegMonitor
 from m1n1.utils import Register64, chexdump
 from m1n1.trace.dart import DARTTracer
-from m1n1.trace.asc import ASCTracer, R_MESSAGE, msg, msg_log, DIR
+from m1n1.trace.asc import ASCTracer, EP, msg, msg_log, DIR
 
 DARTTracer = DARTTracer._reloadcls()
 ASCTracer = ASCTracer._reloadcls()
@@ -29,11 +29,6 @@ DCP_OK =    0x0000000000000242
 # Response to post_presnt
 PRESENT_3_RESP = 0x0000062800000302
 
-class MSG_DCP_CMD(R_MESSAGE):
-    ARG        = 47, 16
-    UNK        = 15, 8
-    ID         = 7, 0
-
 KNOWN_MSGS = {
     PRE_PRESENT: "Pre present",
     PRESENT: "Present",
@@ -43,37 +38,121 @@ KNOWN_MSGS = {
     PRESENT_3_RESP: "Frame presented",
 }
 
-mon = RegMonitor(hv.u)
+iomon = RegMonitor(hv.u, ascii=True)
+
+class IOEpMessage(Register64):
+    TYPE = 63, 48
+
+class IOEp_Generic(IOEpMessage):
+    ARG3 = 47, 32
+    ARG2 = 31, 16
+    ARG1 = 15, 0
+
+class IOEp_SetBuf_Ack(IOEpMessage):
+    UNK1 = 47, 32
+    IOVA = 31, 0
+
+class IOEp_Cmd(IOEpMessage):
+    SIZE = 31, 0
+
+class IOEp(EP):
+    BASE_MESSAGE = IOEp_Generic
+
+    def add_mon(self):
+        if self.state.shmem_iova:
+            iomon.add(self.state.shmem_iova, 32768,
+                      name=f"{self.name}.shmem@{self.state.shmem_iova:08x}", offset=0)
+
+    Init =          msg_log(0x80, DIR.TX)
+    Init_Ack =      msg_log(0xa0, DIR.RX)
+
+    GetBuf =        msg_log(0x89, DIR.RX)
+
+    @msg(0xa1, DIR.TX, IOEp_SetBuf_Ack)
+    def GetBuf_Ack(self, msg):
+        self.state.shmem_iova = msg.IOVA
+        self.add_mon()
+
+    Ack =           msg_log(0x85, DIR.RX)
+
+    @msg(0xa2, DIR.TX, IOEp_SetBuf_Ack)
+    def Cmd(self, msg):
+        pass
+        #mon.poll()
+
+class DCPMessage(Register64):
+    TYPE        = 7, 0
+
+class DCPEp_SetShmem(DCPMessage):
+    IOVA        = 47, 16
+
+class DCPEp_Cmd(DCPMessage):
+    LEN         = 63, 32
+    UNK         = 31, 8
+
+class DCPEp(EP):
+    BASE_MESSAGE = DCPEp_Cmd
+
+    def add_mon(self):
+        iomon.add(self.state.shmem_iova, 16384*32,
+                  name=f"{self.name}.shmem@{self.state.shmem_iova:08x}", offset=0)
+
+    @msg(0x40, DIR.TX, DCPEp_SetShmem)
+    def SetShmem(self, msg):
+        self.log(f"Shared memory IOVA: {msg.IOVA:#x}")
+        self.state.shmem_iova = msg.IOVA
+        self.add_mon()
+
+    @msg(None, DIR.TX, DCPEp_Cmd)
+    def Cmd(self, msg):
+        self.log(f"Cmd: {msg} ({KNOWN_MSGS.get(msg.value, 'unk')})")
+        return True
+
+    @msg(None, DIR.RX, DCPEp_Cmd)
+    def Reply(self, msg):
+        self.log(f"Reply: {msg} ({KNOWN_MSGS.get(msg.value, 'unk')})")
+        return True
 
 class DCPTracer(ASCTracer):
-    @msg(0x37, 0x0, DIR.TX, MSG_DCP_CMD)
-    def DCP_CMD(self, r0, r1):
-        self.log(f"DCP_CMD: {r0} ({KNOWN_MSGS.get(r0.value, 'unk')})")
-        if r0.ID == 0x40:
-            self.log(f"Shared memory IOVA: {r0.ARG:#x}")
-            self.state.shmem_iova = r0.ARG
-            self.state.shmem_phys = self.dart.iotranslate(0, self.state.shmem_iova, 16384)[0][0]
-            mon.add(self.state.shmem_phys, 16384)
-        #chexdump(self.dart.ioread(0, self.state.shmem_iova, 0x1000).rstrip(b"\x00"))
-        mon.poll()
-        #self.hv.run_shell()
+    ENDPOINTS = {
+        0x20: IOEp,
+        0x21: IOEp,
+        0x22: IOEp,
+        0x23: IOEp,
+        0x24: IOEp,
+        0x25: IOEp,
+        0x26: IOEp,
+        0x27: IOEp,
+        0x28: IOEp,
+        0x29: IOEp,
+        0x2a: IOEp,
+        0x2b: IOEp,
+        0x2c: IOEp,
+        0x2d: IOEp,
+        0x37: DCPEp,
+    }
 
-    @msg(0x37, 0x0, DIR.RX, MSG_DCP_CMD)
-    def DCP_REPLY(self, r0, r1):
-        self.log(f"DCP_REPLY: {r0} ({KNOWN_MSGS.get(r0.value, 'unk')})")
-        #self.hv.run_shell()
-        #chexdump(self.dart.ioread(0, self.state.shmem_iova, 0x1000).rstrip(b"\x00"))
-        mon.poll()
-        return False
+    def handle_msg(self, direction, r0, r1):
+        super().handle_msg(direction, r0, r1)
+        iomon.poll()
 
 dart_dcp_tracer = DARTTracer(hv, "/arm-io/dart-dcp")
 dart_dcp_tracer.start()
 
+def readmem_iova(addr, size):
+    try:
+        return dart_dcp_tracer.dart.ioread(0, addr, size)
+    except Exception as e:
+        print(e)
+        return None
+
+iomon.readmem = readmem_iova
+
 dcp_tracer = DCPTracer(hv, "/arm-io/dcp", verbose=1)
 dcp_tracer.start(dart_dcp_tracer.dart)
 
-try:
-    dcp_tracer.state.shmem_phys = dcp_tracer.dart.iotranslate(0, dcp_tracer.state.shmem_iova, 16384)[0][0]
-    mon.add(dcp_tracer.state.shmem_phys, 16384)
-except:
-    pass
+for i in dcp_tracer.epmap.values():
+    try:
+        i.add_mon()
+    except:
+        pass
--- proxyclient/m1n1/trace/asc.py
+++ proxyclient/m1n1/trace/asc.py
@@ -16,9 +16,6 @@ class R_OUTBOX_CTRL(Register32):
 class R_INBOX_CTRL(Register32):
     ENABLE = 1
 
-class R_MESSAGE(Register64):
-    TYPE    = 59, 52
-
 class R_INBOX1(Register64):
     EP      = 7, 0
 
@@ -32,47 +29,15 @@ class R_OUTBOX1(Register64):
 class ASCRegs(RegMap):
     INBOX_CTRL  = 0x8110, R_INBOX_CTRL
     OUTBOX_CTRL = 0x8114, R_OUTBOX_CTRL
-    INBOX0      = 0x8800, R_MESSAGE
+    INBOX0      = 0x8800, Register64
     INBOX1      = 0x8808, R_INBOX1
-    OUTBOX0     = 0x8830, R_MESSAGE
+    OUTBOX0     = 0x8830, Register64
     OUTBOX1     = 0x8838, R_OUTBOX1
 
-# Management messages
-
-class MSG_EP_MAP(R_MESSAGE):
-    LAST    = 51
-    BASE    = 34, 32
-    BITMAP  = 31, 0
-
-class MSG_EP_MAP_ACK(R_MESSAGE):
-    LAST    = 51
-    BASE    = 34, 32
-    MORE    = 0
-
-class MSG_START_EP(R_MESSAGE):
-    EP      = 39, 32
-
-class MSG_START_SYSLOG(R_MESSAGE):
-    UNK1 = 15, 0
-
-# Syslog messages
-
-class MSG_SYSLOG_INIT(R_MESSAGE):
-    BUFSIZE = 7, 0
-
-class MSG_SYSLOG_GET_BUF(R_MESSAGE):
-    UNK1 = 51, 44
-    UNK2 = 39, 32
-    IOVA = 31, 0
-
-class MSG_SYSLOG_LOG(R_MESSAGE):
-    INDEX = 7, 0
-
-def msg(channel, message, direction=None, regtype=None, name=None):
+def msg(message, direction=None, regtype=None, name=None):
     def f(x):
         x.is_message = True
         x.direction = direction
-        x.channel = channel
         x.message = message
         x.regtype = regtype
         x.name = name
@@ -80,32 +45,85 @@ def msg(channel, message, direction=None, regtype=None, name=None):
     return f
 
 def msg_log(*args, **kwargs):
-    def x(self, r0, r1):
+    def x(self, msg):
         return False
     return msg(*args, **kwargs)(x)
 
 def msg_ign(*args, **kwargs):
-    def x(self, r0, r1):
+    def x(self, msg):
         return True
     return msg(*args, **kwargs)(x)
 
+class EPState(object):
+    pass
+
 class EP(object):
+    NAME = None
+    BASE_MESSAGE = None
+
     def __init__(self, tracer, epid):
         self.tracer = tracer
         self.epid = epid
+        self.present = False
         self.started = False
+        self.name = self.NAME or type(self).__name__.lower()
+        self.state = EPState()
+        self.hv = self.tracer.hv
+        self.msgmap = {}
+        for name in dir(self):
+            i = getattr(self, name)
+            if not callable(i) or not getattr(i, "is_message", False):
+                continue
+            self.msgmap[i.direction, i.message] = getattr(self, name), name, i.regtype
 
-class ASCTracer(ADTDevTracer):
+    def log(self, msg):
+        self.tracer.log(f"[{self.name}] {msg}")
+
+    def handle_msg(self, direction, r0, r1):
+        msgtype = None
+        if self.BASE_MESSAGE:
+            r0 = self.BASE_MESSAGE(r0.value)
+            msgtype = r0.TYPE
+
+        handler = None
+        name = "<unknown>"
+        regtype = None
+
+        msgids = [
+            (direction, msgtype),
+            (None, msgtype),
+            (direction, None),
+            (None, None),
+        ]
+
+        for msgid in msgids:
+            handler, name, regtype = self.msgmap.get(msgid, (None, None, None))
+            if handler:
+                break
+
+        if regtype is not None:
+            r0 = regtype(r0.value)
+
+        if handler:
+            if handler.name is not None:
+                name = handler.name
+            if handler(r0):
+                return True
+
+        d = ">" if direction == DIR.TX else "<"
+        self.log(f"{d}{msgtype:#x}({name}) {r0.value:016x} ({r0.str_fields()})")
+        return True
+
+class EPContainer(object):
+    pass
+
+class BaseASCTracer(ADTDevTracer):
     DEFAULT_MODE = TraceMode.SYNC
 
     REGMAPS = [ASCRegs, None]
     NAMES = ["asc", None]
 
-    EP_MGMT = 0
-    EP_CRASHLOG = 1
-    EP_SYSLOG = 2
-    EP_KDEBUG = 3
-    EP_IOREPORTING = 4
+    ENDPOINTS = {}
 
     def w_OUTBOX_CTRL(self, val):
         self.log(f"OUTBOX_CTRL = {val!s}")
@@ -128,32 +146,12 @@ class ASCTracer(ADTDevTracer):
         self.state.ep = {}
 
     def handle_msg(self, direction, r0, r1):
-        msgids = [
-            (direction, r1.EP, r0.TYPE),
-            (None, r1.EP, r0.TYPE),
-            (direction, r1.EP, None),
-            (None, r1.EP, None),
-            (direction, None, r0.TYPE),
-            (None, None, r0.TYPE),
-            (None, None, None),
-        ]
-        handler = None
-
-        for msgid in msgids:
-            handler, name, regtype = self.msgmap.get(msgid, (None, None, None))
-            if handler:
-                break
-        assert handler
-
-        if regtype is not None:
-            r0 = regtype(r0.value)
-
-        if handler.name is not None:
-            name = handler.name
+        if r1.EP in self.epmap:
+            if self.epmap[r1.EP].handle_msg(direction, r0, r1):
+                return
 
         d = ">" if direction == DIR.TX else "<"
-        if not handler(r0, r1):
-            self.log(f" {d}{r1.EP:02x}:{r0.TYPE:02x} {name} {r0.value:016x} ({r0.str_fields()})")
+        self.log(f"{d}{r1.EP:02x} {r0.value:016x} ({r0.str_fields()})")
 
     def start(self, dart=None):
         super().start()
@@ -163,52 +161,127 @@ class ASCTracer(ADTDevTracer):
             i = getattr(self, name)
             if not callable(i) or not getattr(i, "is_message", False):
                 continue
-            self.msgmap[i.direction, i.channel, i.message] = getattr(self, name), name, i.regtype
+            self.msgmap[i.direction, i.endpoint, i.message] = getattr(self, name), name, i.regtype
 
-    unknown = msg_log(None, None, None, name="<unknown>")
+        self.epmap = {}
+        self.ep = EPContainer()
+        for cls in type(self).mro():
+            eps = getattr(cls, "ENDPOINTS", None)
+            if eps is None:
+                break
+            for k, v in eps.items():
+                if k in self.epmap:
+                    continue
+                ep = v(self, k)
+                ep.dart = dart
+                self.epmap[k] = ep
+                if k in self.state.ep:
+                    ep.state.__dict__.update(self.state.ep[k])
+                self.state.ep[k] = ep.state.__dict__
+                if getattr(self.ep, ep.name, None):
+                    ep.name = f"{ep.name}{k:02x}"
+                setattr(self.ep, ep.name, ep)
 
-    # Management operations
+# System endpoints
 
-    HELLO =     msg_log(EP_MGMT, 1, DIR.RX)
-    HELLO_ACK = msg_log(EP_MGMT, 2, DIR.TX)
+class SystemMessage(Register64):
+    TYPE    = 59, 52
 
-    @msg(EP_MGMT, 5, DIR.TX, MSG_START_EP)
-    def START_EP(self, r0, r1):
-        self.state.ep[r0.EP].started = True
+## Management endpoint
 
-    INIT = msg_log(EP_MGMT, 6, DIR.TX)
+class Mgmt_EPMap(SystemMessage):
+    LAST    = 51
+    BASE    = 34, 32
+    BITMAP  = 31, 0
 
-    @msg(EP_MGMT, 8, DIR.RX, MSG_EP_MAP)
-    def EP_MAP(self, r0, r1):
-        for i in range(32):
-            if r0.BITMAP & (1 << i):
-                ep = 32 * r0.BASE + i
-                self.log(f"  Registering endpoint #{ep:#02x}")
-                if ep not in self.state.ep:
-                    self.state.ep[ep] = EP(self, ep)
+class Mgmt_EPMap_Ack(SystemMessage):
+    LAST    = 51
+    BASE    = 34, 32
+    MORE    = 0
 
-    EP_MAP_ACK = msg_log(EP_MGMT, 8, DIR.TX, MSG_EP_MAP_ACK)
+class Mgmt_StartEP(SystemMessage):
+    EP      = 39, 32
 
-    START_SYSLOG = msg_log(EP_MGMT, 0x0b, DIR.TX, MSG_START_SYSLOG)
-    START_SYSLOG_ACK = msg_log(EP_MGMT, 0x0b, DIR.RX, MSG_START_SYSLOG)
+class Mgmt_StartSyslog(SystemMessage):
+    UNK1 = 15, 0
 
-    SYSLOG_INIT = msg_log(EP_SYSLOG, 8, DIR.RX)
-    SYSLOG_GET_BUF_REQ = msg_log(EP_SYSLOG, 1, DIR.RX, MSG_SYSLOG_GET_BUF)
+class Management(EP):
+    BASE_MESSAGE = SystemMessage
 
-    @msg(EP_SYSLOG, 1, DIR.TX, MSG_SYSLOG_GET_BUF)
-    def SYSLOG_GET_BUF_ACK(self, r0, r1):
-        self.state.ep[r1.EP].syslog_buf = r0.IOVA
+    HELLO =     msg_log(1, DIR.RX)
+    HELLO_ACK = msg_log(2, DIR.TX)
 
-    @msg(EP_SYSLOG, 5, DIR.RX, MSG_SYSLOG_LOG)
-    def SYSLOG_LOG(self, r0, r1):
-        if self.dart is None:
-            return False
-        buf = self.state.ep[r1.EP].syslog_buf
-        log = self.dart.ioread(0, buf + r0.INDEX * 0xa0, 0xa0)
-        hdr, unk, context, msg = struct.unpack("<II24s128s", log)
-        context = context.rstrip(b"\x00").decode("ascii")
-        msg = msg.rstrip(b"\x00").decode("ascii").rstrip("\n")
-        self.log(f"syslog: [{context}]{msg}")
+    @msg(5, DIR.TX, Mgmt_StartEP)
+    def StartEP(self, msg):
+        ep = self.tracer.epmap.get(msg.EP, None)
+        if ep:
+            ep.started = True
+            self.log(f"  Starting endpoint #{msg.EP:#02x} ({ep.name})")
+        else:
+            self.log(f"  Starting endpoint #{msg.EP:#02x}")
         return True
 
-    SYSLOG_LOG_ACK = msg_ign(EP_SYSLOG, 5, DIR.TX, MSG_SYSLOG_LOG)
+    Init = msg_log(6, DIR.TX)
+
+    @msg(8, DIR.RX, Mgmt_EPMap)
+    def EPMap(self, msg):
+        for i in range(32):
+            if msg.BITMAP & (1 << i):
+                epno = 32 * msg.BASE + i
+                ep = self.tracer.epmap.get(epno, None)
+                if ep:
+                    ep.present = True
+                    self.log(f"  Adding endpoint #{epno:#02x} ({ep.name})")
+                else:
+                    self.log(f"  Adding endpoint #{epno:#02x}")
+
+    EPMap_Ack = msg_log(8, DIR.TX, Mgmt_EPMap_Ack)
+
+    StartSyslog = msg_log(0x0b, DIR.TX, Mgmt_StartSyslog)
+    StartSyslog_Ack = msg_log(0x0b, DIR.RX, Mgmt_StartSyslog)
+
+## Syslog endpoint
+
+class Syslog_Init(SystemMessage):
+    BUFSIZE = 7, 0
+
+class Syslog_GetBuf(SystemMessage):
+    UNK1 = 51, 44
+    UNK2 = 39, 32
+    IOVA = 31, 0
+
+class Syslog_Log(SystemMessage):
+    INDEX = 7, 0
+
+class Syslog(EP):
+    BASE_MESSAGE = SystemMessage
+
+    Init = msg_log(8, DIR.RX, Syslog_Init)
+    GetBuf = msg_log(1, DIR.RX, Syslog_GetBuf)
+
+    @msg(1, DIR.TX, Syslog_GetBuf)
+    def GetBuf_Ack(self, msg):
+        self.state.syslog_buf = msg.IOVA
+
+    @msg(5, DIR.RX, Syslog_Log)
+    def Log(self, msg):
+        if self.tracer.dart is None:
+            return False
+        buf = self.state.syslog_buf
+        log = self.tracer.dart.ioread(0, buf + msg.INDEX * 0xa0, 0xa0)
+        hdr, unk, context, logmsg = struct.unpack("<II24s128s", log)
+        context = context.rstrip(b"\x00").decode("ascii")
+        logmsg = logmsg.rstrip(b"\x00").decode("ascii").rstrip("\n")
+        self.log(f"* [{context}]{logmsg}")
+        return True
+
+    Log_Ack = msg_ign(5, DIR.TX, Syslog_Log)
+
+class ASCTracer(BaseASCTracer):
+    ENDPOINTS = {
+        0: Management,
+        #1: CrashLog,
+        2: Syslog,
+        #3: KDebug,
+        #4: IOReporting,
+    }
