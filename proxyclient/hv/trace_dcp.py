819d2cf6bcfea8a042c961ecc34e084b64aab5ca
Hector Martin <marcan@marcan.st>
2021-06-24 01:15:32 +0900

    hv/trace_dcp.py: Parsing a bunch of stuff now
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/hv/trace_dcp.py
+++ proxyclient/hv/trace_dcp.py
@@ -1,42 +1,16 @@
 # SPDX-License-Identifier: MIT
 
+import struct
+
 trace_device("/arm-io/dcp", True, ranges=[1])
 
 from m1n1.proxyutils import RegMonitor
-from m1n1.utils import Register64, chexdump
+from m1n1.utils import *
 from m1n1.trace.dart import DARTTracer
-from m1n1.trace.asc import ASCTracer, EP, msg, msg_log, DIR
+from m1n1.trace.asc import ASCTracer, EP, EPState, msg, msg_log, DIR
 
 DARTTracer = DARTTracer._reloadcls()
 ASCTracer = ASCTracer._reloadcls()
-
-# Sequence of commands sent to endpoint 0x37 to present a frame The new
-# framebuffer becomes visible after sending PRESENT, confirmed by timing.
-PRE_PRESENT  = 0x0000003c00000202
-PRESENT      = 0x000008b400000202
-POST_PRESENT = 0x0000000000000342
-
-# Sent repeatedly when reconfiguring the display (clicking anything in the
-# Displays section of System Preferences -- scaling, rotation, colour profile,
-# all included). The actual command is in shared memory. Acked by the DCP with
-# an OK. This command is _not_ sufficient for hardware resolution changes, only
-# for software (perceived) changes.
-RECONFIGURE  = 0x0000106c00000202
-
-# Response to pre_present, present, reconfigure
-DCP_OK =    0x0000000000000242
-
-# Response to post_presnt
-PRESENT_3_RESP = 0x0000062800000302
-
-KNOWN_MSGS = {
-    PRE_PRESENT: "Pre present",
-    PRESENT: "Present",
-    POST_PRESENT: "Post present",
-    RECONFIGURE: "Reconfigure",
-    DCP_OK: "DCP OK",
-    PRESENT_3_RESP: "Frame presented",
-}
 
 iomon = RegMonitor(hv.u, ascii=True)
 
@@ -52,11 +26,65 @@ class IOEp_SetBuf_Ack(IOEpMessage):
     UNK1 = 47, 32
     IOVA = 31, 0
 
-class IOEp_Cmd(IOEpMessage):
-    SIZE = 31, 0
+class IOEp_Send(IOEpMessage):
+    WPTR = 31, 0
+
+class IORingBuf(Reloadable):
+    def __init__(self, ep, state, base):
+        self.ep = ep
+        self.dart = ep.dart
+        self.state = state
+        self.base = base
+        self.align = 0x40
+
+    def init(self):
+        self.state.bufsize, unk = struct.unpack("<II", self.dart.ioread(0, self.base, 8))
+        self.state.rptr = 0
+
+    def read(self, max=None, wptr=None):
+        wptr2 = struct.unpack("<I", self.dart.ioread(0, self.base + 0x80, 4))[0]
+        assert wptr is None or wptr == wptr2
+
+        rptr = self.state.rptr
+        while wptr2 != rptr:
+            hdr = self.dart.ioread(0, self.base + 0xc0 + rptr, 16)
+            rptr += 16
+            magic, size = struct.unpack("<4sI", hdr[:8])
+            assert magic == b"IOP "
+            if size > (self.state.bufsize - rptr - 16):
+                hdr = self.dart.ioread(0, self.base + 0xc0, 16)
+                rptr = 16
+                magic, size = struct.unpack("<4sI", hdr[:8])
+                assert magic == b"IOP "
+
+            payload = self.dart.ioread(0, self.base + 0xc0 + rptr, size)
+            rptr = (align_up(rptr + size, self.align)) % self.state.bufsize
+            self.state.rptr = rptr
+            yield hdr[8:] + payload
+            if max is not None:
+                max -= 1
+                if max <= 0:
+                    break
 
 class IOEp(EP):
     BASE_MESSAGE = IOEp_Generic
+
+    def __init__(self, tracer, epid):
+        super().__init__(tracer, epid)
+        self.state.txbuf = EPState()
+        self.state.rxbuf = EPState()
+        self.state.shmem_iova = None
+        self.state.verbose = 1
+
+    def start(self):
+        #self.add_mon()
+        self.create_bufs()
+
+    def create_bufs(self):
+        if not self.state.shmem_iova:
+            return
+        self.txbuf = IORingBuf(self, self.state.txbuf, self.state.shmem_iova)
+        self.rxbuf = IORingBuf(self, self.state.rxbuf, self.state.shmem_iova + 0x4000)
 
     def add_mon(self):
         if self.state.shmem_iova:
@@ -71,70 +99,240 @@ class IOEp(EP):
     @msg(0xa1, DIR.TX, IOEp_SetBuf_Ack)
     def GetBuf_Ack(self, msg):
         self.state.shmem_iova = msg.IOVA
-        self.add_mon()
+        #self.add_mon()
 
-    Ack =           msg_log(0x85, DIR.RX)
+    @msg(0xa2, DIR.TX, IOEp_Send)
+    def Send(self, msg):
+        for data in self.txbuf.read(wptr=msg.WPTR):
+            if self.state.verbose >= 1:
+                self.log(f">TX rptr={self.txbuf.state.rptr:#x}")
+                chexdump(data)
+        return True
 
-    @msg(0xa2, DIR.TX, IOEp_SetBuf_Ack)
-    def Cmd(self, msg):
-        pass
-        #mon.poll()
+    Hello =         msg_log(0xa3, DIR.TX)
+
+    @msg(0x85, DIR.RX, IOEpMessage)
+    def Recv(self, msg):
+        for data in self.rxbuf.read():
+            if self.state.verbose >= 1:
+                self.log(f"<RX rptr={self.rxbuf.state.rptr:#x}")
+                chexdump(data)
+        return True
+
+    @msg(0x8b, DIR.RX)
+    def BufInitialized(self, msg):
+        self.create_bufs()
+        self.txbuf.init()
+        self.rxbuf.init()
 
 class DCPMessage(Register64):
-    TYPE        = 7, 0
+    TYPE        = 3, 0
+    CMD_EMPTY   = 6
 
 class DCPEp_SetShmem(DCPMessage):
     IOVA        = 47, 16
 
-class DCPEp_Cmd(DCPMessage):
+class TxOp(IntEnum):
+    ACK_REPLY = 0
+    CMD =       2
+    ACK_ASYNC = 3
+    CMD_ALT   = 6
+
+class RxOp(IntEnum):
+    REPLY =     0
+    ACK_CMD =   2
+    ASYNC =     3
+    ACK_ALT =   6
+
+class DCPEp_Tx(DCPMessage):
     LEN         = 63, 32
-    UNK         = 31, 8
+    OP          = 11, 8, TxOp
+
+class DCPEp_Rx(DCPMessage):
+    LEN         = 63, 32
+    OP          = 11, 8, RxOp
+
+KNOWN_MSGS = {
+    "D581": "Cursor updated",
+    "D589": "Frame presented",
+    "A407": "Pre present",
+    "A408": "Present",
+    "A435": "Set Processing",
+    "A422": "Set Night Shift",
+}
 
 class DCPEp(EP):
-    BASE_MESSAGE = DCPEp_Cmd
+    BASE_MESSAGE = DCPMessage
+
+    def __init__(self, tracer, epid):
+        super().__init__(tracer, epid)
+        self.state.shmem_iova = None
+        self.state.show_globals = True
+        self.state.show_acks = True
+        self.state.max_len = 1024 * 1024
+        self.state.verbosity = 2
+        self.state.op_verb = {}
+
+    def start(self):
+        self.add_mon()
 
     def add_mon(self):
-        iomon.add(self.state.shmem_iova, 16384*32,
-                  name=f"{self.name}.shmem@{self.state.shmem_iova:08x}", offset=0)
+        if self.state.shmem_iova and self.state.show_globals:
+            addr = self.state.shmem_iova + 0x80000
+            iomon.add(addr, 128,
+                      name=f"{self.name}.shmem@{addr:08x}", offset=addr)
 
-    @msg(0x40, DIR.TX, DCPEp_SetShmem)
+    InitComplete = msg_log(1, DIR.RX)
+
+    @msg(0, DIR.TX, DCPEp_SetShmem)
     def SetShmem(self, msg):
         self.log(f"Shared memory IOVA: {msg.IOVA:#x}")
         self.state.shmem_iova = msg.IOVA
         self.add_mon()
 
-    @msg(None, DIR.TX, DCPEp_Cmd)
-    def Cmd(self, msg):
-        self.log(f"Cmd: {msg} ({KNOWN_MSGS.get(msg.value, 'unk')})")
+    @msg(2, DIR.TX, DCPEp_Tx)
+    def Tx(self, msg):
+        if msg.LEN > 0:
+            assert msg.OP in (TxOp.CMD, TxOp.CMD_ALT)
+            addr = self.state.shmem_iova
+            if msg.OP == TxOp.CMD_ALT:
+                addr += 0x8000
+            data = self.dart.ioread(0, addr, msg.LEN)
+            tag, din, dout = self.dec_msg(data)
+            if msg.OP == TxOp.CMD_ALT:
+                self.state.alt_info = tag, din, dout
+                self.state.alt_len = msg.LEN
+            else:
+                self.state.cmd_info = tag, din, dout
+                self.state.cmd_len = msg.LEN
+
+            verb = self.get_verbosity(tag)
+            if verb >= 1:
+                self.log(f">{msg.OP.name} {tag}:{KNOWN_MSGS.get(tag, 'unk')} ({msg})")
+            if verb >= 2:
+                self.show_msg(tag, din, dout)
+        else:
+            if self.state.show_acks:
+                self.log(f">{msg.OP.name} ({msg})")
+
         return True
 
-    @msg(None, DIR.RX, DCPEp_Cmd)
-    def Reply(self, msg):
-        self.log(f"Reply: {msg} ({KNOWN_MSGS.get(msg.value, 'unk')})")
+    @msg(2, DIR.RX, DCPEp_Rx)
+    def Rx(self, msg):
+        if msg.OP in (RxOp.ACK_CMD, RxOp.ACK_ALT, RxOp.REPLY):
+            if msg.OP in(RxOp.ACK_CMD, RxOp.REPLY):
+                addr = self.state.shmem_iova
+                size = self.state.cmd_len
+                tag, din, dout = self.state.cmd_info
+            elif msg.OP == RxOp.ACK_ALT:
+                addr = self.state.shmem_iova + 0x8000
+                size = self.state.alt_len
+                tag, din, dout = self.state.alt_info
+            verb = self.get_verbosity(tag)
+            if verb >= 3 and len(dout) > 0:
+                dout2 = self.dart.ioread(0, addr + 12 + len(din), len(dout))
+                print(f"< Output buffer ({len(dout2):#x} bytes):")
+                chexdump(dout2[:self.state.max_len])
+        if msg.LEN > 0:
+            addr = self.state.shmem_iova
+            if msg.OP == RxOp.ASYNC:
+                addr += 0x40000
+            else:
+                addr += 0x60000
+            data = self.dart.ioread(0, addr, msg.LEN)
+            tag, din, dout = self.dec_msg(data)
+            verb = self.get_verbosity(tag)
+            if verb >= 1:
+                self.log(f"<{msg.OP.name} {tag}:{KNOWN_MSGS.get(tag, 'unk')} ({msg})")
+            if verb >= 2:
+                self.show_msg(tag, din, dout)
+
+        if self.state.show_globals:
+            iomon.poll()
         return True
+
+    def get_verbosity(self, tag):
+        return self.state.op_verb.get(tag, self.state.verbosity)
+
+    def set_verb_known(self, verb):
+        for i in KNOWN_MSGS:
+            if verb is None:
+                self.state.op_verb.pop(i, None)
+            else:
+                self.state.op_verb[i] = verb
+
+    def dec_msg(self, data):
+        tag = data[:4][::-1].decode("ascii")
+        size_in, size_out = struct.unpack("<II", data[4:12])
+        data_in = data[12:12+size_in]
+        data_out = data[12+size_in:12+size_in+size_out]
+        assert len(data) == (size_in + size_out + 12)
+        return tag, data_in, data_out
+
+    def show_msg(self, tag, data_in, data_out):
+        print(f"Message: {tag} ({KNOWN_MSGS.get(tag, 'unk')}): (in {len(data_in):#x}, out {len(data_out):#x})")
+        if data_in:
+            print(f"> Input ({len(data_in):#x} bytes):")
+            chexdump(data_in[:self.state.max_len])
+        #if data_out:
+            #print(f"> Output buffer ({len(data_out):#x} bytes):")
+            #chexdump(data_out[:self.state.max_len])
+
+class SystemService(IOEp):
+    NAME = "system"
+
+class TestService(IOEp):
+    NAME = "test"
+
+class DCPExpertService(IOEp):
+    NAME = "dcpexpert"
+
+class Disp0Service(IOEp):
+    NAME = "disp0"
+
+class DPTXService(IOEp):
+    NAME = "dptx"
+
+class DPSACService(IOEp):
+    NAME = "dpsac"
+
+class DPDevService(IOEp):
+    NAME = "dpdev"
+
+class MDCP29XXService(IOEp):
+    NAME = "mcdp29xx"
+
+class AVService(IOEp):
+    NAME = "av"
+
+class HDCPService(IOEp):
+    NAME = "hdcp"
+
+class RemoteAllocService(IOEp):
+    NAME = "remotealloc"
 
 class DCPTracer(ASCTracer):
     ENDPOINTS = {
-        0x20: IOEp,
-        0x21: IOEp,
-        0x22: IOEp,
-        0x23: IOEp,
-        0x24: IOEp,
+        0x20: SystemService,
+        0x21: TestService,
+        0x22: DCPExpertService,
+        0x23: Disp0Service,
+        0x24: DPTXService,
         0x25: IOEp,
-        0x26: IOEp,
-        0x27: IOEp,
-        0x28: IOEp,
-        0x29: IOEp,
+        0x26: DPSACService,
+        0x27: DPDevService,
+        0x28: MDCP29XXService,
+        0x29: AVService,
         0x2a: IOEp,
-        0x2b: IOEp,
+        0x2b: HDCPService,
         0x2c: IOEp,
-        0x2d: IOEp,
+        0x2d: RemoteAllocService,
         0x37: DCPEp,
     }
 
     def handle_msg(self, direction, r0, r1):
         super().handle_msg(direction, r0, r1)
-        iomon.poll()
+        #iomon.poll()
 
 dart_dcp_tracer = DARTTracer(hv, "/arm-io/dart-dcp")
 dart_dcp_tracer.start()
@@ -150,9 +348,3 @@ iomon.readmem = readmem_iova
 
 dcp_tracer = DCPTracer(hv, "/arm-io/dcp", verbose=1)
 dcp_tracer.start(dart_dcp_tracer.dart)
-
-for i in dcp_tracer.epmap.values():
-    try:
-        i.add_mon()
-    except:
-        pass
