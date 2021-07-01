5ac520f010c1c773b8c209058b045e40f0369ffd
Sven Peter <sven@svenpeter.dev>
2021-07-01 20:17:28 +0200

    kboot: move SEPFW to initrd before booting
    Bootstrapping SEPOS requires to setup a scratch buffer and
    will have to be handled by the final OS. We pass along
    the firmware image by appending it to the initramfs.
    
    Signed-off-by: Sven Peter <sven@svenpeter.dev>

--- src/kboot.h
+++ src/kboot.h
@@ -19,6 +19,7 @@ struct kernel_header {
 
 void kboot_set_initrd(void *start, size_t size);
 void kboot_set_bootargs(const char *ba);
+int kboot_prepare_fw(void);
 int kboot_prepare_dt(void *fdt);
 int kboot_boot(void *kernel);
 
