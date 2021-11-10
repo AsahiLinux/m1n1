9d2be299a410be65f199672ff2bf80346328c9be
Hector Martin <marcan@marcan.st>
2021-11-10 16:36:45 +0900

    tools/smccli.py: Simple shell for an instantiated SMCClient
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ proxyclient/tools/smccli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+import sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
+from m1n1.setup import *
+from m1n1.shell import run_shell
+from m1n1.fw.smc import SMCClient
+
+smc_addr = u.adt["arm-io/smc"].get_reg(0)[0]
+smc = SMCClient(u, smc_addr, None)
+
+smc.start()
+smc.start_ep(0x20)
+
+run_shell(globals(), msg="Have fun!")
