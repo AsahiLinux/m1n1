927c12ad5310613cb9b04ab21e8fdc6e5cb376d2
Hector Martin <marcan@marcan.st>
2021-11-10 16:35:21 +0900

    hv/trace_all.py: Trace all I/O ranges using the arm-io prop
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/hv/trace_all.py
+++ proxyclient/hv/trace_all.py
@@ -1,7 +1,10 @@
 # SPDX-License-Identifier: MIT
 
+from m1n1.utils import irange
+
 # Map the entire MMIO range as traceable
-trace_range(range(0x2_00000000, 0x7_00000000))
+for r in hv.adt["/arm-io"].ranges:
+    trace_range(irange(r.parent_addr, r.size), mode=TraceMode.SYNC)
 
 # Skip some noisy devices
 try:
@@ -12,7 +15,10 @@ try:
     trace_device("/arm-io/usb-drd1", False)
 except KeyError:
     pass
-trace_device("/arm-io/uart2", False)
+try:
+    trace_device("/arm-io/uart2", False)
+except KeyError:
+    pass
 trace_device("/arm-io/error-handler", False)
 trace_device("/arm-io/aic", False)
 trace_device("/arm-io/spi1", False)
