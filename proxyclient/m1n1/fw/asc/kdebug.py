0e3ee31254d76d9fcfdf28967e071791249be2cb
Hector Martin <marcan@marcan.st>
2021-11-23 17:20:26 +0900

    m1n1.fw.asc.kdebug: Handle prealloc messages (partially)
    Not sure if these need a reply...
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/m1n1/fw/asc/kdebug.py
+++ proxyclient/m1n1/fw/asc/kdebug.py
@@ -9,6 +9,15 @@ class KDebugGetBufMessage(KDebugMessage):
     TYPE    = 55, 48, Constant(1)
     COUNT   = 47, 0
 
+class KDebugPreallocBuf1Message(KDebugMessage):
+    TYPE    = 55, 48, Constant(2)
+    DVA     = 47, 12
+    FLAGS   = 11, 0
+
+class KDebugPreallocBuf2Message(KDebugMessage):
+    TYPE    = 55, 48, Constant(3)
+    DVA     = 47, 0
+
 class KDebugSendBufMessage(KDebugMessage):
     TYPE    = 55, 48
     DVA     = 47, 0
@@ -28,6 +37,16 @@ class ASCKDebugEndpoint(ASCBaseEndpoint):
 
         self.iobuffer1, self.iobuffer1_iova = self.asc.ioalloc(0x2000)
         self.send(KDebugSendBufMessage(TYPE=2, DVA=self.iobuffer1_iova))
+        return True
+
+    @msg_handler(2, KDebugPreallocBuf1Message)
+    def SetBuf1(self, msg):
+        #self.send(KDebugSendBufMessage(TYPE=1, DVA=msg.DVA))
+        return True
+
+    @msg_handler(3, KDebugPreallocBuf2Message)
+    def SetBuf2(self, msg):
+        #self.send(KDebugSendBufMessage(TYPE=2, DVA=msg.DVA))
         return True
 
     def start(self):
