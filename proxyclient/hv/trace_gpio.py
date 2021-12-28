ba258dc805ce11fd586f997eb8003bb30949e218
Hector Martin <marcan@marcan.st>
2021-12-28 21:09:30 +0900

    hv/trace_gpio.py: Work on both t8103 and t6000
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/hv/trace_gpio.py
+++ proxyclient/hv/trace_gpio.py
@@ -6,11 +6,17 @@ from m1n1.trace.gpio import GPIOTracer
 
 # trace gpio interrups, useful to follow the cascaded interrupts
 aic_phandle = getattr(hv.adt["/arm-io/aic"], "AAPL,phandle")
-node = hv.adt["/arm-io/gpio"]
+try:
+    node = hv.adt["/arm-io/gpio0"]
+    path = "/arm-io/gpio0"
+except:
+    node = hv.adt["/arm-io/gpio"]
+    path = "/arm-io/gpio"
+
 if getattr(node, "interrupt-parent") == aic_phandle:
     for irq in getattr(node, "interrupts"):
         hv.trace_irq(node.name, irq, 1, hv.IRQTRACE_IRQ)
 
 GPIOTracer = GPIOTracer._reloadcls()
-gpio_tracer = GPIOTracer(hv, "/arm-io/gpio", verbose=0)
+gpio_tracer = GPIOTracer(hv, path, verbose=0)
 gpio_tracer.start()
