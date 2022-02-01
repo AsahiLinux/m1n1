317a40e0b22ae4697738330b5e0791ee5f1c5363
Finn Behrens <me@kloenk.dev>
2022-02-01 09:25:21 +0100

    Use macro to generate write functions
    Signed-off-by: Finn Behrens <me@kloenk.dev>

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,4 +1,13 @@
-#[inline]
-pub unsafe fn write32(addr: u64, data: u32) {
-    unsafe { core::ptr::write_volatile(addr as *mut _, data) };
+macro_rules! gen_write {
+    ($name:tt, $size:tt) => {
+        #[inline]
+        pub unsafe fn $name(addr: u64, data: $size) {
+            unsafe { core::ptr::write_volatile(addr as *mut _, data) };
+        }
+    };
 }
+
+gen_write!(write64, u64);
+gen_write!(write32, u32);
+gen_write!(write16, u16);
+gen_write!(write8, u8);
