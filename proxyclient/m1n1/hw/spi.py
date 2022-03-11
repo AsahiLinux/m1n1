be1421d2addf87a8750013d01d1fb467746fc5ed
Hector Martin <marcan@marcan.st>
2022-03-11 22:14:46 +0900

    m1n1.hw.spi: Update register definitions
    This had been sitting in my worktree...
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/m1n1/hw/spi.py
+++ proxyclient/m1n1/hw/spi.py
@@ -1,7 +1,4 @@
 # SPDX-License-Identifier: MIT
-
-import struct
-
 from ..utils import *
 
 __all__ = ["SPIRegs"]
@@ -11,11 +8,7 @@ class R_CTRL(Register32):
     TX_FIFO_RESET   = 2
     RUN             = 0
 
-class R_PIN(Register32):
-    CS              = 1
-    KEEP_MOSI       = 0
-
-class R_CONFIG(Register32):
+class R_CFG(Register32):
     # impl: 002fb1e6
     IE_TX_COMPLETE  = 21
     b19             = 19
@@ -43,32 +36,33 @@ class R_STATUS(Register32):
     TXRX_THRESH     = 1     # updated if MODE == 1
     RX_COMPLETE     = 0
 
-class R_FIFO_STAT(Register32):
+class R_PIN(Register32):
+    CS              = 1
+    KEEP_MOSI       = 0
+
+class R_CLKDIV(Register32):
+    DIVIDER         = 10, 0 # SPI freq = CLK / (DIVIDER + 1)
+
+class R_INTER_DELAY(Register32):
+    DELAY           = 15, 0
+
+class R_FIFOSTAT(Register32):
     LEVEL_RX        = 31, 24
     RX_EMPTY        = 20
     LEVEL_TX        = 15, 8
     TX_FULL         = 4
 
-class R_ISTATUS1(Register32):
+class R_IRQ_XFER(Register32):
     TX_XFER_DONE    = 1
     RX_XFER_DONE    = 0
 
-class R_ISTATUS2(Register32):
+class R_IRQ_FIFO(Register32):
     TX_OVERFLOW     = 17
     RX_UNDERRUN     = 16
     TX_EMPTY        = 9
     RX_FULL         = 8
     TX_THRESH       = 5
     RX_THRESH       = 4
-
-class R_CLKDIV(Register32):
-    DIVIDER         = 10, 0 # SPI freq = CLK / (DIVIDER + 1)
-
-class R_INTER_DLY(Register32):
-    DELAY           = 15, 0
-
-# FIFO size: 16 bytes
-# backend FIFO: TX 2 bytes, RX 1 byte?
 
 class R_XFSTATUS(Register32):
     SR_FULL         = 26
@@ -80,7 +74,7 @@ class R_DIVSTATUS(Register32):
     COUNT2          = 31, 16
     COUNT1          = 15, 0
 
-class R_SHIFTCONFIG(Register32):
+class R_SHIFTCFG(Register32):
     OVERRIDE_CS     = 24
     BITS            = 21, 16
     RX_ENABLE       = 11
@@ -91,7 +85,7 @@ class R_SHIFTCONFIG(Register32):
     CS_ENABLE       = 1
     CLK_ENABLE      = 0
 
-class R_PINCONFIG(Register32):
+class R_PINCFG(Register32):
     MOSI_INIT_VAL   = 10
     CS_INIT_VAL     = 9
     CLK_INIT_VAL    = 8
@@ -110,42 +104,42 @@ class R_DELAY(Register32):
     NO_INTERBYTE    = 1
     ENABLE          = 0
 
-class R_SCK_CONFIG(Register32):
+class R_SCKCFG(Register32):
     PERIOD          = 31, 16
     PHASE1          = 9
     PHASE0          = 8
     RESET_TO_IDLE   = 4
 
-class R_SCK_PHASES(Register32):
+class R_SCKPHASES(Register32):
     PHASE1_START   = 31, 16
     PHASE0_START   = 15, 0
 
 class SPIRegs(RegMap):
     CTRL        = 0x00, R_CTRL
-    CONFIG      = 0x04, R_CONFIG
+    CFG         = 0x04, R_CFG
     STATUS      = 0x08, R_STATUS
     PIN         = 0x0C, R_PIN
     TXDATA      = 0x10, Register32
     RXDATA      = 0x20, Register32
     CLKDIV      = 0x30, R_CLKDIV
     RXCNT       = 0x34, Register32
-    INTER_DLY   = 0x38, R_INTER_DLY
+    INTER_DELAY = 0x38, R_INTER_DELAY
     TXCNT       = 0x4C, Register32
-    FIFO_STAT   = 0x10C, R_FIFO_STAT
+    FIFOSTAT    = 0x10C, R_FIFOSTAT
 
-    IMASK1      = 0x130, R_ISTATUS1
-    ISTATUS1    = 0x134, R_ISTATUS1
-    IMASK2      = 0x138, R_ISTATUS2
-    ISTATUS2    = 0x13c, R_ISTATUS2
+    IE_XFER     = 0x130, R_IRQ_XFER
+    IF_XFER     = 0x134, R_IRQ_XFER
+    IE_FIFO     = 0x138, R_IRQ_FIFO
+    IF_FIFO     = 0x13c, R_IRQ_FIFO
 
-    SHIFTCONFIG = 0x150, R_SHIFTCONFIG
-    PINCONFIG   = 0x154, R_PINCONFIG
+    SHIFTCFG    = 0x150, R_SHIFTCFG
+    PINCFG      = 0x154, R_PINCFG
 
-    PRE_DLY     = 0x160, R_DELAY
-    SCK_CONFIG  = 0x164, R_SCK_CONFIG
-    POST_DLY    = 0x168, R_DELAY
+    DELAY_PRE   = 0x160, R_DELAY
+    SCKCFG      = 0x164, R_SCKCFG
+    DELAY_POST  = 0x168, R_DELAY
 
-    SCK_PHASES  = 0x180, R_SCK_PHASES
+    SCKPHASES  = 0x180, R_SCKPHASES
 
     UNK_PHASE   = 0x18c, Register32 # probably MISO sample point
 
