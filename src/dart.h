9266ed3bb9eab9efcfb67cf36a62a25decc36dbb
Janne Grunau <j@jannau.net>
2022-03-29 13:10:32 +0200

    dcp: Free iovad_dcp including dart L2 tables on shutdown
    Signed-off-by: Janne Grunau <j@jannau.net>

--- src/dart.h
+++ src/dart.h
@@ -11,6 +11,7 @@ dart_dev_t *dart_init(uintptr_t base, u8 device, bool keep_pts);
 dart_dev_t *dart_init_adt(const char *path, int instance, int device, bool keep_pts);
 int dart_map(dart_dev_t *dart, uintptr_t iova, void *bfr, size_t len);
 void dart_unmap(dart_dev_t *dart, uintptr_t iova, size_t len);
+void dart_free_l2(dart_dev_t *dart, uintptr_t iova);
 void *dart_translate(dart_dev_t *dart, uintptr_t iova);
 u64 dart_search(dart_dev_t *dart, void *paddr);
 s64 dart_find_iova(dart_dev_t *dart, s64 start, size_t len);
