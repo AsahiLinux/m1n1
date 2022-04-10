a5089dae08f785ee30b01f24fbd28de75d9d9413
R <rqou@berkeley.edu>
2022-04-09 18:33:51 -0700

    bluetooth: dump firmware, a little bit of context
    Signed-off-by: R <rqou@berkeley.edu>

--- proxyclient/hv/trace_bt.py
+++ proxyclient/hv/trace_bt.py
@@ -88,7 +88,7 @@ class MemRangeTracer(Tracer):
 
 
 class BTTracer(MemRangeTracer):
-    DEFAULT_MODE = TraceMode.ASYNC
+    DEFAULT_MODE = TraceMode.SYNC
     REGMAPS = [BTBAR0Regs, BTBAR1Regs]
     # FIXME this is kinda a hack
     REGRANGES = [(0x5c2410000, 0x8000), (0x5c2000000, 0x400000)]
@@ -97,11 +97,105 @@ class BTTracer(MemRangeTracer):
     def __init__(self, hv, dart_tracer):
         super().__init__(hv, verbose=3)
         self.dart_tracer = dart_tracer
+        self._dart_tracer_started = False
+        self._dumped_host_window = False
+        self._dump_idx = 0
+        self._host_window_lo = 0
+        self._host_window_hi = 0
+        self._host_window_sz = 0
+        self._rti_window_lo = 0
+        self._rti_window_hi = 0
+        self._rti_window_sz = 0
+        self._rti_context_lo = 0
+        self._rti_context_hi = 0
+
+    def w_DOORBELL_6(self, val):
+        # this is the first access macos does
+        if not self._dart_tracer_started:
+            self.dart_tracer.start()
+            self._dart_tracer_started = True
+            print("starting DART tracer")
+
+    def w_HOST_WINDOW_LO(self, val):
+        self._host_window_lo = val
+
+    def w_HOST_WINDOW_HI(self, val):
+        self._host_window_hi = val
+
+    def w_HOST_WINDOW_SZ(self, val):
+        self._host_window_sz = val
+
+    def w_IMG_DOORBELL(self, val):
+        print("Image doorbell was rung")
+        host_window_iova = int(self._host_window_hi) << 32 | int(self._host_window_lo)
+        host_window_sz = int(self._host_window_sz)
+        print(f"Host window @ {host_window_iova:016X} sz {host_window_sz:08X}")
+
+        self.dart_tracer.dart.dump_all()
+
+        # FIXME how do we get stream IDs without hardcoding?
+        try:
+            data = self.dart_tracer.dart.ioread(2, host_window_iova, host_window_sz)
+            with open(f'bt_dump_{self._dump_idx}.bin', 'wb') as f:
+                f.write(data)
+            chexdump(data[:0x400])
+            self._dump_idx += 1
+        except Exception as e:
+            print(e)
+
+
+    def w_RTI_WINDOW_LO(self, val):
+        self._rti_window_lo = val
+
+    def w_RTI_WINDOW_HI(self, val):
+        self._rti_window_hi = val
+
+    def w_RTI_WINDOW_SZ(self, val):
+        self._rti_window_sz = val
+
+    def w_RTI_CONTEXT_LO(self, val):
+        self._rti_context_lo = val
+
+    def w_RTI_CONTEXT_HI(self, val):
+        self._rti_context_hi = val
+
+    def w_RTI_CONTROL(self, val):
+        print("RTI control was set")
+        host_window_iova = int(self._host_window_hi) << 32 | int(self._host_window_lo)
+        host_window_sz = int(self._host_window_sz)
+        print(f"Host window @ {host_window_iova:016X} sz {host_window_sz:08X}")
+        rti_window_iova = int(self._rti_window_hi) << 32 | int(self._rti_window_lo)
+        rti_window_sz = int(self._rti_window_sz)
+        print(f"RTI window @ {rti_window_iova:016X} sz {rti_window_sz:08X}")
+        rti_context_iova = int(self._rti_context_hi) << 32 | int(self._rti_context_lo)
+        print(f"RTI context @ {rti_context_iova:016X}")
+
+        self.dart_tracer.dart.dump_all()
+
+        # FIXME how do we get stream IDs without hardcoding?
+        try:
+            data = self.dart_tracer.dart.ioread(2, host_window_iova, host_window_sz)
+            with open(f'bt_dump_{self._dump_idx}.bin', 'wb') as f:
+                f.write(data)
+            chexdump(data[:0x400])
+            self._dump_idx += 1
+        except Exception as e:
+            print(e)
+
+        try:
+            data = self.dart_tracer.dart.ioread(2, rti_context_iova, 0x100)
+            with open(f'bt_dump_{self._dump_idx}.bin', 'wb') as f:
+                f.write(data)
+            chexdump(data)
+            self._dump_idx += 1
+        except Exception as e:
+            print(e)
 
 
 BTTracer = BTTracer._reloadcls()
 
 dart_tracer = DARTTracer(hv, "/arm-io/dart-apcie0", verbose=1)
+dart_tracer.DEFAULT_MODE = TraceMode.SYNC
 # do not start, clock gates aren't enabled yet
 print(dart_tracer)
 
