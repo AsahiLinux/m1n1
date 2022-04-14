3440aadc3c70829673dd2045ccdcc7531577504e
Akihiko Odaki <akihiko.odaki@gmail.com>
2022-04-15 07:07:58 +0900

    proxy: Specify coordinates in fb_fill method
    Signed-off-by: Akihiko Odaki <akihiko.odaki@gmail.com>

--- proxyclient/m1n1/proxy.py
+++ proxyclient/m1n1/proxy.py
@@ -1004,7 +1004,7 @@ class M1N1Proxy(Reloadable):
         return self.request(self.P_FB_BLIT, x, y, w, h, ptr, stride)
     def fb_unblit(self, x, y, w, h, ptr, stride):
         return self.request(self.P_FB_UNBLIT, x, y, w, h, ptr, stride)
-    def fb_fill(self, color):
+    def fb_fill(self, x, y, w, h, color):
         return self.request(self.P_FB_FILL, x, y, w, h, color)
     def fb_clear(self, color):
         return self.request(self.P_FB_CLEAR, color)
