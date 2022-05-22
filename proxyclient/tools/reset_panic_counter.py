12db5c3910309d021d9c3a94bc1593f67a97abdd
Janne Grunau <j@jannau.net>
2022-05-22 08:46:13 +0200

    tools/reset_panic_counter.py: Use ADT to support t600x
    In addition make it executable and add python shebang as for the other
    tools.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- proxyclient/tools/reset_panic_counter.py
+++ proxyclient/tools/reset_panic_counter.py
@@ -6,6 +6,16 @@ sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 from m1n1.setup import *
 from m1n1.hw.spmi import SPMI
 
-s = SPMI(u, "/arm-io/nub-spmi")
-leg_scrpad = 0x9f00
+compatible = u.adt["/arm-io"].compatible[0]
+
+if compatible == "arm-io,t6000":
+    s = SPMI(u, "/arm-io/nub-spmi0")
+    leg_scrpad = u.adt["/arm-io/nub-spmi0/spmi-mpmu"].info_leg__scrpad[0]
+elif compatible == "arm-io,t8103":
+    s = SPMI(u, "/arm-io/nub-spmi")
+    leg_scrpad = u.adt["/arm-io/nub-spmi/spmi-pmu"].info_leg__scrpad[0]
+else:
+    print(f"Unknown SoC comaptible '{compatible}'")
+    exit()
+
 s.write8(15, leg_scrpad + 2, 0) # error counts
