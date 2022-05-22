8a90ced4b41dc8dc622903925832174fb8e2f27e
Janne Grunau <j@jannau.net>
2022-05-22 08:55:30 +0200

    tools/reset_panic_counter.py: minor fixes
     - add SPDX-License-Identifier
     - make it executable
     - add python shebang
     - remove unused import
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- proxyclient/tools/reset_panic_counter.py
+++ proxyclient/tools/reset_panic_counter.py
@@ -1,4 +1,6 @@
-import sys, pathlib, time
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+import sys, pathlib
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 
 from m1n1.setup import *
