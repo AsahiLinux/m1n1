d7a4f300430cc996e42c6de9c5d71d60e9419c9d
Asahi Lina <lina@asahilina.net>
2022-06-02 23:28:25 +0900

    m1n1.agx: Use new UAT init API
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- proxyclient/m1n1/agx/__init__.py
+++ proxyclient/m1n1/agx/__init__.py
@@ -21,20 +21,15 @@ class AGX:
         self.fw_va_base = self.sgx_dev.rtkit_private_vm_region_base
         self.fw_va_size = self.sgx_dev.rtkit_private_vm_region_size
         self.kern_va_base = self.fw_va_base + self.fw_va_size
-        self.ttbr_base = self.sgx_dev.gpu_region_base
-        self.ttbr0_base = u.memalign(self.PAGE_SIZE, self.PAGE_SIZE)
-        self.ttbr1_base = self.sgx_dev.gfx_shared_region_base
 
         # Set up UAT
         self.uat = UAT(self.u.iface, self.u)
+        self.uat.early_init()
+
         # Allocator for RTKit/ASC objects
         self.uat.allocator = Heap(self.kern_va_base + 0x80000000,
                                   self.kern_va_base + 0x81000000,
                                   self.PAGE_SIZE)
-        self.uat.set_ttbr(self.ttbr_base)
-        self.uat.set_l0(0, 0, self.ttbr0_base)
-        self.uat.set_l0(0, 1, self.ttbr1_base)
-        self.uat.flush_dirty()
 
         self.asc = AGXASC(self.u, self.asc_dev.get_reg(0)[0], self, self.uat)
         self.asc.verbose = 0
