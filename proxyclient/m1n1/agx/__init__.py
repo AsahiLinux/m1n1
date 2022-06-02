722f4c4b8ea9c8133e61fffa737f196d036edf68
Asahi Lina <lina@asahilina.net>
2022-06-02 23:37:10 +0900

    m1n1.agx: Add channel allocation/init
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- proxyclient/m1n1/agx/__init__.py
+++ proxyclient/m1n1/agx/__init__.py
@@ -7,6 +7,13 @@ from .channels import *
 from ..malloc import Heap
 from ..hw.uat import UAT, MemoryAttr
 from ..fw.agx import AGXASC
+from ..fw.agx.channels import ChannelInfoSet, ChannelInfo
+
+class AGXChannels:
+    pass
+
+class AGXQueue:
+    pass
 
 class AGX:
     PAGE_SIZE = 0x4000
@@ -73,17 +80,96 @@ class AGX:
         if self.mon is not None:
             obj.add_to_mon(self.mon)
 
-    def boot(self):
-        # boot asc
-        super().boot()
+    def alloc_channels(self, cls, name, channel_id, count=1, rx=False):
 
-        initdata_addr = self.build_initdata()
-        self.agx.send_initdata(self.initdata_addr)
+        # All channels have 0x100 items
+        item_count = 0x100
+        item_size = cls.item_size
+        ring_size = item_count * item_size
+
+        self.log(f"Allocating {count} channel(s) for {name} ({item_count} * {item_size:#x} bytes each)")
+
+        state_obj = self.kshared.new_buf(0x30 * count, f"Channel.{name}.state")
+        if rx:
+            ring_buf = self.kshared.new_buf(ring_size * count, f"Channel.{name}.ring")
+        else:
+            ring_buf = self.kobj.new_buf(ring_size * count, f"Channel.{name}.ring")
+
+        info = ChannelInfo()
+        info.state_addr = state_obj._addr
+        info.ringbuffer_addr = ring_buf._addr
+        setattr(self.ch_info, name, info)
+
+        return [cls(self, name + ("" if count == 1 else f"[{i}]"), channel_id,
+                    state_obj._paddr + 0x30 * i,
+                    ring_buf._paddr + ring_size * i, item_count)
+                for i in range(count)]
+
+    def init_channels(self):
+        self.log("Initializing channels...")
+        self.ch_info = ChannelInfoSet()
+        self.ch = AGXChannels()
+        self.ch.queue = []
+
+        # Command queue submission channels
+        for index in range(4):
+            queue = AGXQueue()
+            self.ch.queue.append(queue)
+            for typeid, chtype in enumerate(("TA", "3D", "CL")):
+                name = f"{chtype}_{index}"
+                chan = self.alloc_channels(GPUCmdQueueChannel, name,
+                                           (index << 2) | typeid)[0]
+                setattr(queue, "q_" + chtype, chan)
+
+        # Device control channel
+        self.ch.devctrl = self.alloc_channels(GPUDeviceControlChannel, "DevCtrl", 0x11)[0]
+
+        # GPU -> CPU channels
+        self.ch.event = self.alloc_channels(GPUEventChannel, "Event", None)[0]
+        self.ch.log = self.alloc_channels(GPULogChannel, "FWLog", None, 6)
+        self.ch.ktrace = self.alloc_channels(GPUKTraceChannel, "KTrace", None)[0]
+        self.ch.stats = self.alloc_channels(GPUStatsChannel, "Stats", None)[0]
+
+        # For some reason, the FWLog channels have their rings in a different place...
+        self.fwlog_ring = self.ch_info.FWLog.ringbuffer_addr
+        self.ch_info.FWLog.ringbuffer_addr = self.kshared.buf(0x150000, "FWLog_Dummy")
+
+    def poll_channels(self):
+        self.ch.event.poll()
+        for chan in self.ch.log:
+            chan.poll()
+        self.ch.ktrace.poll()
+        self.ch.stats.poll()
+
+    def kick_firmware(self):
+        self.asc.db.doorbell(0x10)
 
     def start(self):
+        self.log("Starting ASC")
         self.asc.start()
 
+        self.log("Starting endpoints")
+        self.asc.start_ep(0x20)
+        self.asc.start_ep(0x21)
+
+        self.init_channels()
+
+        self.log("Building initdata")
         self.initdata = build_initdata(self)
+        self.uat.flush_dirty()
+
+        self.log("Sending initdata")
+        self.asc.fw.send_initdata(self.initdata._addr & 0xfff_ffffffff)
+        self.asc.work()
+
+        self.log("Sending DC19")
+        self.ch.devctrl.send_dc19()
+        self.asc.work()
+
+        for i in range(4):
+            self.log("Sending DC23")
+            self.ch.devctrl.send_dc23()
+            self.asc.work()
 
     def stop(self):
         self.asc.stop()
