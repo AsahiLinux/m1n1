d603cf3178c35614da93a775486c82be9ff22d88
Janne Grunau <j@jannau.net>
2022-06-05 10:03:45 +0200

    m1n1.hw.pmu: Add SPMI PMU support for resetting the panic counter
    Replace the handrolled implementation in experiments/agx_1tri.py and
    tools/reset_panic_counter.py.
    In addition reset the panic counter in reboot.py and run_guest.py. If
    can reboot or start guests the boot process works fine and macOS
    recovery needs to leave us alone.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- proxyclient/tools/reboot.py
+++ proxyclient/tools/reboot.py
@@ -4,5 +4,8 @@ import sys, pathlib
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 
 from m1n1.setup import *
+from m1n1.hw.pmu import PMU
+
+PMU(u).reset_panic_counter()
 
 p.reboot()
