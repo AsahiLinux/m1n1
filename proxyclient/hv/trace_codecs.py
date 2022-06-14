9cefbd6f679231c9970494c662d25e8dc8ddd98b
Martin Povišer <povik@protonmail.com>
2022-06-14 17:19:09 +0200

    hv/trace_codecs.py: Trace volume/gain on speaker amps
    Signed-off-by: Martin Povišer <povik@protonmail.com>

---
+++ proxyclient/hv/trace_codecs.py
@@ -0,0 +1,38 @@
+# SPDX-License-Identifier: MIT
+
+from m1n1.utils import RegMap
+from m1n1.trace.i2c import I2CTracer, I2CRegMapTracer
+from m1n1.hw.codecs import *
+
+class SN012776Tracer(I2CRegMapTracer):
+    REGMAP = SN012776Regs
+    ADDRESSING = (1, 1)
+
+class TAS5770Tracer(I2CRegMapTracer):
+    REGMAP = TAS5770Regs
+    ADDRESSING = (1, 1)
+
+i2c_tracers = {}
+
+for node in hv.adt["/arm-io"]:
+    if not node.name.startswith("i2c"):
+        continue
+
+    n = int(node.name[3:])
+    i2c_tracers[n] = bus = I2CTracer(hv, f"/arm-io/{node.name}")
+
+    for devnode in node:
+        if "compatible" not in devnode._properties: # thanks Apple
+            continue
+
+        dcls = {
+            "audio-control,tas5770": TAS5770Tracer,
+            "audio-control,sn012776": SN012776Tracer
+        }.get(devnode.compatible[0], None)
+        if dcls:
+            bus.add_device(
+                devnode.reg[0] & 0xff,
+                dcls(name=devnode.name)
+            )
+
+    bus.start()
