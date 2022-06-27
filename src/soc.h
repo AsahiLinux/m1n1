8a607f049ff3e44162b1e4bff542542f4d7dd6b7
Hector Martin <marcan@marcan.st>
2022-06-27 18:16:51 +0900

    soc: Add T8112 defines
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- src/soc.h
+++ src/soc.h
@@ -6,6 +6,7 @@
 #include "../config.h"
 
 #define T8103 0x8103
+#define T8112 0x8112
 #define T6000 0x6000
 #define T6001 0x6001
 #define T6002 0x6002
@@ -16,6 +17,8 @@
 #define EARLY_UART_BASE 0x235200000
 #elif TARGET == T6000 || TARGET == T6001 || TARGET == T6002
 #define EARLY_UART_BASE 0x39b200000
+#elif TARGET == T8112
+#define EARLY_UART_BASE 0x235200000
 #endif
 
 #endif
