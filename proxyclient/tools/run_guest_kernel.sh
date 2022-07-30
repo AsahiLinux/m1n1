12bff05700c43a055e1b0e49445433ada58012b1
Hector Martin <marcan@marcan.st>
2022-07-30 14:28:29 +0900

    tools/run_guest_kernel.sh: New utility script
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ proxyclient/tools/run_guest_kernel.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+if [ ! -d "$1" ]; then
+    echo "Usage:"
+    echo "  $0 <kernel build root> [kernel commandline] [initramfs]"
+    exit 1
+fi
+
+kernel_base="$(realpath "$1")"
+args="$2"
+initramfs=""
+if [ ! -z "$3" ]; then
+    initramfs="$(realpath "$3")"
+fi
+
+cd "$(dirname "$0")"
+
+echo "Creating m1n1+kernel image"
+cp ../../build/m1n1.bin /tmp/m1n1-linux.bin
+if [ ! -z "$args" ]; then
+    echo "chosen.bootargs=$args" >>/tmp/m1n1-linux.bin
+fi
+
+cat "$kernel_base"/arch/arm64/boot/dts/apple/*.dtb "$kernel_base"/arch/arm64/boot/Image.gz >>/tmp/m1n1-linux.bin
+echo "Chainloading to updated m1n1..."
+python chainload.py -r ../../build/m1n1.bin
+echo "Starting guest..."
+exec python run_guest.py -c "load_system_map('$kernel_base/System.map')" -r /tmp/m1n1-linux.bin
