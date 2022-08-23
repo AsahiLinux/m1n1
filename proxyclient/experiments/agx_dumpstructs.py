d1a3b8c7fae6e9b2d89b2b6e686d90aa3af6103b
Asahi Lina <lina@asahilina.net>
2022-08-23 15:32:45 +0900

    experiments/agx_dumpstructs.py: Dump microsequence
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- proxyclient/experiments/agx_dumpstructs.py
+++ proxyclient/experiments/agx_dumpstructs.py
@@ -5,11 +5,15 @@ import sys, pathlib, time
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 
 from m1n1.constructutils import *
-from m1n1.fw.agx import initdata
+from m1n1.fw.agx import microsequence, initdata
 
 #for v in initdata.__all__:
-for v in initdata.__dict__:
-    struct = getattr(initdata, v)
-    if isinstance(struct, type) and issubclass(struct, ConstructClass) and struct is not ConstructClass:
-        print(struct.to_rust())
-        print()
+#for v in initdata.__dict__:
+def dump(module):
+    for v in module.__dict__:
+        struct = getattr(module, v)
+        if isinstance(struct, type) and issubclass(struct, ConstructClass) and struct is not ConstructClass:
+            print(struct.to_rust())
+            print()
+
+dump(microsequence)
