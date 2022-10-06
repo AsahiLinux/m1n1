a8ef753520a20c603f7cedcf0322cf39b0138196
Martin Povišer <povik@protonmail.com>
2022-10-06 23:30:01 +0200

    m1n1.fw.aop: In AOPBase only refer to ProxyUtils attribute
    Signed-off-by: Martin Povišer <povik@protonmail.com>

--- proxyclient/m1n1/fw/aop/__init__.py
+++ proxyclient/m1n1/fw/aop/__init__.py
@@ -10,18 +10,18 @@ class AOPBase:
 
     @property
     def _bootargs_span(self):
-        base = self.fw_base + self.p.read32(self.fw_base + 0x224)
-        length = self.p.read32(self.fw_base + 0x228)
+        base = self.fw_base + self.u.proxy.read32(self.fw_base + 0x224)
+        length = self.u.proxy.read32(self.fw_base + 0x228)
 
         return (base, length)
 
     def read_bootargs(self):
-        blob = self.iface.readmem(*self._bootargs_span)
+        blob = self.u.proxy.iface.readmem(*self._bootargs_span)
         return ASCArgumentSection(blob)
 
     def write_bootargs(self, args):
         base, _ = self._bootargs_span
-        self.iface.writemem(base, args.to_bytes())
+        self.u.proxy.iface.writemem(base, args.to_bytes())
 
     def update_bootargs(self, keyvals):
         args = self.read_bootargs()
