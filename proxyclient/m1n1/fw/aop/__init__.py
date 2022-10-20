e7b16164c9ab023068d2dba8f743b3a14f399fdf
Martin Povišer <povik@protonmail.com>
2022-10-20 23:44:46 +0200

    m1n1.fw.aop: Move bootargs helpers to new class AOPBase
    The new class can possibly be shared between the m1n1 AOP tracer and
    AOP client.
    
    Signed-off-by: Martin Povišer <povik@protonmail.com>

--- proxyclient/m1n1/fw/aop/__init__.py
+++ proxyclient/m1n1/fw/aop/__init__.py
@@ -1,4 +1,31 @@
 # SPDX-License-Identifier: MIT
 from .bootargs import ASCArgumentSection
 
-__all__ = ["ASCArgumentSection"]
+class AOPBase:
+    def __init__(self, u, adtnode):
+        self.fw_base, self.fw_len = adtnode.get_reg(2)
+        if u.adt["arm-io"].compatible[0] == "arm-io,t6000":
+            # argh
+            self.fw_base -= 0x2_0000_0000
+
+    @property
+    def _bootargs_span(self):
+        base = self.fw_base + self.p.read32(self.fw_base + 0x224)
+        length = self.p.read32(self.fw_base + 0x228)
+
+        return (base, length)
+
+    def read_bootargs(self):
+        blob = self.iface.readmem(*self._bootargs_span)
+        return ASCArgumentSection(blob)
+
+    def write_bootargs(self, args):
+        base, _ = self._bootargs_span
+        self.iface.writemem(base, args.to_bytes())
+
+    def update_bootargs(self, keyvals):
+        args = self.read_bootargs()
+        args.update(keyvals)
+        self.write_bootargs(args)
+
+__all__ = ["ASCArgumentSection", "AOPBase"]
