6e99fde8f2896f850b31dd0f099a71be875335d9
Asahi Lina <lina@asahilina.net>
2022-10-29 21:40:22 +0900

    hv/trace_all_more.py: Trace more stuff
    Signed-off-by: Asahi Lina <lina@asahilina.net>

---
+++ proxyclient/hv/trace_all_more.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: MIT
+
+from m1n1.utils import irange
+
+# Map the entire MMIO range as traceable
+for r in hv.adt["/arm-io"].ranges:
+    trace_range(irange(r.parent_addr, r.size), mode=TraceMode.ASYNC)
+
+# Skip some noisy devices
+try:
+    trace_device("/arm-io/usb-drd0", False)
+except KeyError:
+    pass
+try:
+    trace_device("/arm-io/usb-drd1", False)
+except KeyError:
+    pass
+try:
+    trace_device("/arm-io/uart2", False)
+except KeyError:
+    pass
+trace_device("/arm-io/aic", False)
+trace_device("/arm-io/spi1", False)
