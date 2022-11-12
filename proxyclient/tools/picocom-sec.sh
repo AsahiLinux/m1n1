21a403a1630aa31eb996fbf509012fc7b8b903ed
Alexis Deruelle <alexis.deruelle@gmail.com>
2022-11-12 15:00:18 +0100

    proxyclient:tools:add default device name for macOS
    Under macOS host, secondary UART device name is different from the linux
    device name. Check OS name and chose default device name accordingly.
    
    Signed-off-by: Alexis Deruelle <alexis.deruelle@gmail.com>

--- proxyclient/tools/picocom-sec.sh
+++ proxyclient/tools/picocom-sec.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
+#
+# Secondary UART device name exposed when m1n1 boots is /dev/m1n1-sec on
+# Linux host and /dev/cu.usbmodemP_03 on macOS host
+#
+DEFAULT_SECDEV=/dev/m1n1-sec
+PLATFORM_NAME=$(uname)
+
+case "${PLATFORM_NAME}" in
+Darwin)
+    SECDEV=/dev/cu.usbmodemP_03 ;;
+*)
+    SECDEV="$DEFAULT_SECDEV" ;;
+esac
+
+# The secondary UART device shows up when m1n1 boots on the tethered machine
+echo "Waiting for UART device file '$SECDEV' to appear (Ctrl+C to abort)..."
 
 while true; do
-    while [ ! -e /dev/m1n1-sec ]; do sleep 1; done
-    picocom --omap crlf --imap lfcrlf -b 500000 /dev/m1n1-sec
+    while [ ! -e $SECDEV ] ; do sleep 1 ; done
+    picocom --omap crlf --imap lfcrlf -b 500000 $SECDEV
     sleep 1
 done
