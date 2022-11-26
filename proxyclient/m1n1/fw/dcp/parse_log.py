de2eea162eb1a0491ca07a4e08b62fd01004d0df
Janne Grunau <j@jannau.net>
2022-11-26 17:49:11 +0100

    m1n1.fw.dcp: Parse AVPropChunks in parse_log
    Signed-off-by: Janne Grunau <j@jannau.net>

--- proxyclient/m1n1/fw/dcp/parse_log.py
+++ proxyclient/m1n1/fw/dcp/parse_log.py
@@ -3,6 +3,34 @@
 from m1n1.utils import *
 from m1n1.fw.dcp.ipc import *
 
+class DCPAVPropHandler:
+    def __init__(self):
+        self.dcpav_prop = {}
+
+    def setDCPAVPropStart(self, length):
+        # print(f"setDCPAVPropStart({length:#x})")
+        self.dcpav_prop_len = length - 1 # off by one?
+        self.dcpav_prop_off = 0
+        self.dcpav_prop_data = []
+        return True
+
+    def setDCPAVPropChunk(self, data, offset, length):
+        # print(f"setDCPAVPropChunk(..., {offset:#x}, {length:#x})")
+        assert offset == self.dcpav_prop_off
+        self.dcpav_prop_data.append(data)
+        self.dcpav_prop_off += len(data)
+        return True
+
+    def setDCPAVPropEnd(self, key):
+        # print(f"setDCPAVPropEnd({key!r})")
+        blob = b"".join(self.dcpav_prop_data)
+        assert self.dcpav_prop_len == len(blob)
+        self.dcpav_prop[key] = OSSerialize().parse(blob)
+        self.dcpav_prop_data = self.dcpav_prop_len = self.dcpav_prop_off = None
+        pprint.pprint(self.dcpav_prop[key])
+        return True
+
+
 def parse_log(fd):
     op_stack = {}
     for line in fd:
@@ -27,8 +55,24 @@ def dump_log(fd):
         "": 0,
         "OOB": 0,
     }
+
+    handler = DCPAVPropHandler()
+
     for op in parse_log(fd):
         ctx = ""
+        if Ver.check("V < V13_5"):
+            dcpavprop_cbs = ["D122", "D123", "D124"]
+        else:
+             dcpavprop_cbs = ["D126", "D127", "D128"]
+        if not op.complete and op.msg in dcpavprop_cbs:
+            method = op.get_method()
+            if op.msg == dcpavprop_cbs[0]:
+                method.callback(handler.setDCPAVPropStart, op.in_data)
+            if op.msg == dcpavprop_cbs[1]:
+                method.callback(handler.setDCPAVPropChunk, op.in_data)
+            if op.msg == dcpavprop_cbs[2]:
+                method.callback(handler.setDCPAVPropEnd, op.in_data)
+
         if "OOB" in op.chan:
             ctx = "[OOB] -----------> "
         if not op.complete:
