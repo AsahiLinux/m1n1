8be0596ae817934d27d0965199c5bb1eef8d2448
Hector Martin <marcan@marcan.st>
2023-03-15 13:54:15 +0900

    experiments/mtp.py: Fix DART stuff
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- proxyclient/experiments/mtp.py
+++ proxyclient/experiments/mtp.py
@@ -7,7 +7,7 @@ sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 import struct
 from m1n1.setup import *
 from m1n1.fw.asc import StandardASC
-from m1n1.hw.dart8110 import DART8110
+from m1n1.hw.dart import DART
 from m1n1.hw.dockchannel import DockChannel
 from m1n1.fw.smc import SMCClient, SMCError
 from m1n1.shell import run_shell
@@ -33,9 +33,9 @@ smc.verbose = 0
 
 p.dapf_init_all()
 
-dart = DART8110.from_adt(u, "/arm-io/dart-mtp", iova_range=(0x8000, 0x100000))
+dart = DART.from_adt(u, "/arm-io/dart-mtp", iova_range=(0x8000, 0x100000))
 
-dart.regs.TCR[1].set(BYPASS_DAPF=0, BYPASS_DART=0, TRANSLATE_ENABLE=1)
+dart.dart.regs.TCR[1].set(BYPASS_DAPF=0, BYPASS_DART=0, TRANSLATE_ENABLE=1)
 
 irq_base = u.adt["/arm-io/dockchannel-mtp"].get_reg(1)[0]
 fifo_base = u.adt["/arm-io/dockchannel-mtp"].get_reg(2)[0]
