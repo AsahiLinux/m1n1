bc7fbc366ecb8905f8231b2aff3673bd3fda7cb6
Kristoffer Grundström <swedishsailfishosuser@tutanota.com>
2023-04-09 23:25:02 +0200

    Make version.sh to work in more shells
    I'm NOT an expert, but I'd like to see this being a reality so that more shells can run this script.
    This should help make it more universal.

--- version.sh
+++ version.sh
@@ -5,7 +5,7 @@ dirbase="$(basename "$(pwd)")"
 
 if [ -n "$M1N1_VERSION_TAG" ]; then
     version="$M1N1_VERSION_TAG"
-elif [ -e ".git" ]; then
+elif command -v git >/dev/null 2>&1 && [ -d ".git" ]; then
     version="$(git describe --tags --always --dirty)"
 elif [ "$(echo "${dirbase}" | cut -c1-5)" = "m1n1-" ]; then
     version=$(echo "${dirbase}" | cut -c6-)
@@ -14,4 +14,16 @@ else
     version="unknown"
 fi
 
-echo "#define BUILD_TAG \"$version\""
+# Check which shell is running
+if [ -n "${BASH_VERSION:-}" ]; then
+    echo "#define BUILD_TAG \"$version\""
+elif [ -n "${ZSH_VERSION:-}" ]; then
+    printf '#define BUILD_TAG "%s"\n' "$version"
+elif [ -n "${KSH_VERSION:-}" ]; then
+    echo "#define BUILD_TAG=\"$version\""
+elif [ -n "${POSIXLY_CORRECT:-}" ] || [ "$(basename "$0")" = "sh" ]; then
+    echo "#define BUILD_TAG=\"$version\""
+else
+    echo "Unsupported shell."
+    exit 1
+fi
