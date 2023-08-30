84c0aa10bb39ddbedf2026610f26107267ecb3bf
Janne Grunau <j@jannau.net>
2023-08-30 08:33:48 +0200

    afk: Sync structs/names with proxyclient's python definitions
    Signed-off-by: Janne Grunau <j@jannau.net>

--- src/afk.h
+++ src/afk.h
@@ -11,7 +11,7 @@ afk_epic_ep_t *afk_epic_init(rtkit_dev_t *rtkit, int endpoint);
 int afk_epic_shutdown(afk_epic_ep_t *epic);
 
 int afk_epic_start_interface(afk_epic_ep_t *epic, char *name, size_t insize, size_t outsize);
-int afk_epic_command(afk_epic_ep_t *epic, int channel, u16 code, void *txbuf, size_t txsize,
+int afk_epic_command(afk_epic_ep_t *epic, int channel, u16 sub_type, void *txbuf, size_t txsize,
                      void *rxbuf, size_t *rxsize);
 
 #endif
