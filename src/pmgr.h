ca3ca11a41612cb0f08adda9452f068df47a2a5c
Hector Martin <marcan@marcan.st>
2023-09-08 00:04:29 +0900

    pmgr: Expose pmgr_set_mode
    For ISP stuff which is not declared in the ADT
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- src/pmgr.h
+++ src/pmgr.h
@@ -10,6 +10,10 @@
 #define PMGR_DEVICE_ID GENMASK(15, 0)
 #define PMGR_DIE_ID    GENMASK(31, 28)
 
+#define PMGR_PS_ACTIVE  0xf
+#define PMGR_PS_CLKGATE 0x4
+#define PMGR_PS_PWRGATE 0x0
+
 int pmgr_init(void);
 
 int pmgr_power_enable(u32 id);
@@ -22,6 +26,8 @@ int pmgr_adt_power_disable_index(const char *path, u32 index);
 int pmgr_adt_reset(const char *path);
 
 int pmgr_reset(int die, const char *name);
+
+int pmgr_set_mode(uintptr_t addr, u8 target_mode);
 
 u32 pmgr_get_feature(const char *name);
 
