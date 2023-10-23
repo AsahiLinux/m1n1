64a16048205d367a824793b11d5695d16b3c5c31
Tobias Heider <me@tobhe.de>
2023-10-23 20:29:24 +0200

    bash is not available everywhere. Use #!/usr/bin/env sh instead which should work on most posix platforms. Fixes OpenBSD.
    Signed-off-by: Tobias Heider <me@tobhe.de>

--- data/makelogo.sh
+++ data/makelogo.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env sh
 convert bootlogo_128.png -background black -flatten -depth 8 rgba:bootlogo_128.bin
 convert bootlogo_256.png -background black -flatten -depth 8 rgba:bootlogo_256.bin
