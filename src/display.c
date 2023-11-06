4d5b143ff9cde4e355e39a5fd5f0aadb7b3af4bb
Janne Grunau <j@jannau.net>
2023-11-06 18:29:58 +0100

    display: Optionally use dcpext0/1 on t6020 and t6021
    Allows emulating a t6022 system with fused off dcp0/dcp1 display
    controllers.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- src/display.c
+++ src/display.c
@@ -48,15 +48,33 @@ static const display_config_t display_config_m2 = {
     .num_dptxports = 2,
 };
 
+#define T6020_T6021_USE_DCPEXT 255
+
 static const display_config_t display_config_m2_pro_max = {
+#if T6020_T6021_USE_DCPEXT == 0
+    .dcp = "/arm-io/dcpext0",
+    .dcp_dart = "/arm-io/dart-dcpext0",
+    .disp_dart = "/arm-io/dart-dispext0",
+    .pmgr_dev = "DISPEXT0_CPU0",
+    .dcp_index = 1,
+    .num_dptxports = 2,
+#elif T6020_T6021_USE_DCPEXT == 1
+    .dcp = "/arm-io/dcpext1",
+    .dcp_dart = "/arm-io/dart-dcpext1",
+    .disp_dart = "/arm-io/dart-dispext1",
+    .pmgr_dev = "DISPEXT1_CPU0",
+    .dcp_index = 2,
+    .num_dptxports = 2,
+#else
     .dcp = "/arm-io/dcp0",
     .dcp_dart = "/arm-io/dart-dcp0",
     .disp_dart = "/arm-io/dart-disp0",
-    .dp2hdmi_gpio = "/arm-io/dp2hdmi-gpio0",
-    .dptx_phy = "/arm-io/lpdptx-phy0",
     .pmgr_dev = "DISP0_CPU0",
     .dcp_index = 0,
     .num_dptxports = 1,
+#endif
+    .dp2hdmi_gpio = "/arm-io/dp2hdmi-gpio0",
+    .dptx_phy = "/arm-io/lpdptx-phy0",
 };
 
 static const display_config_t display_config_m2_ultra = {
