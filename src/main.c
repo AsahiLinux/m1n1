1dd3e98a4f34206643a937bc6dcdbafb42ebbd5b
Nick Chan <towinchenmi@gmail.com>
2024-08-25 15:22:56 +0800

    Skip over features unsupported in A7-A11 SoCs.
    This includes
    - GXF
    - SPRR
    - PAN (A7-A9)
    - Atomics (A7-A10)
    - FIPI (A7-A10)
    - DCP
    
    SPRR on T8030 seems to be quite different from M1 (at least according to
    qemu-t8030), so keep it disabled for those CPU parts.
    
    Signed-off-by: Nick Chan <towinchenmi@gmail.com>

--- src/main.c
+++ src/main.c
@@ -51,6 +51,7 @@ void get_device_info(void)
         printf("  Target: %s\n", target);
 
     is_mac = !!strstr(model, "Mac");
+    has_dcp = adt_path_offset(adt, "/arm-io/dcp") > 0;
 
     int chosen = adt_path_offset(adt, "/chosen");
     if (chosen > 0) {
@@ -146,13 +147,13 @@ void m1n1_main(void)
 
     printf("Running in EL%lu\n\n", mrs(CurrentEL) >> 2);
 
-    get_device_info();
     firmware_init();
 
     heapblock_init();
 
 #ifndef BRINGUP
-    gxf_init();
+    if (supports_gxf())
+        gxf_init();
     mcc_init();
     mmu_init();
     aic_init();
@@ -160,13 +161,14 @@ void m1n1_main(void)
     wdt_disable();
 #ifndef BRINGUP
     pmgr_init();
-
 #ifdef USE_FB
     display_init();
     // Kick DCP to sleep, so dodgy monitors which cause reconnect cycles don't cause us to lose the
     // framebuffer.
-    display_shutdown(DCP_SLEEP_IF_EXTERNAL);
-    fb_init(false);
+    if (has_dcp)
+        display_shutdown(DCP_SLEEP_IF_EXTERNAL);
+    // On idevice we need to always clear, because otherwise it looks scuffed on white devices
+    fb_init(!is_mac);
     fb_display_logo();
 #ifdef FB_SILENT_MODE
     fb_set_active(!cur_boot_args.video.display);
