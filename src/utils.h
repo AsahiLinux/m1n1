8508526515a9ce6b613e2e0e0373b07793a5c791
James Calligeros <jcalligeros99@gmail.com>
2024-12-21 11:55:01 +1000

    display: contain all display init logic within display.c
    Display handling was spread across main.c, utils.c and display.c
    with the addition of iDevice support. Condense all of this functionality
    into display.c.
    
    This also fixes a regression introduced in 869d2ae35c31 ("Skip over features
    unsupported in A7-A11 SoCs."), which caused devices using dcpext to skip DCP
    initialisation.
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- src/utils.h
+++ src/utils.h
@@ -464,7 +464,7 @@ struct vector_args {
 
 extern u32 board_id, chip_id;
 
-extern bool is_mac, has_dcp;
+extern bool is_mac;
 extern bool cpufeat_actlr_el2, cpufeat_fast_ipi, cpufeat_mmu_sprr;
 extern bool cpufeat_global_sleep, cpufeat_workaround_cyclone_cache;
 
