166893cec09ad71092d7fe68f7387e307cd3d0ea
James Calligeros <jcalligeros99@gmail.com>
2025-03-29 18:18:01 +1000

    adt: replace adt_check_header() with Rust
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- rust/src/adt.rs
+++ rust/src/adt.rs
@@ -150,6 +150,14 @@ unsafe extern "C" {
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn adt_check_header(_dt: *const c_void) -> c_int {
+    match ADT::node_at(0) {
+        Ok(_n) => 0,
+        Err(e) => e as c_int,
+    }
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn adt_first_property_offset(_dt: *const c_void, offset: c_int) -> c_int {
     ADT::first_property_offset(offset) as c_int
 }
