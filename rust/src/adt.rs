e4e97fcdc53e3400fb69dffa00105fd884841cae
James Calligeros <jcalligeros99@gmail.com>
2025-04-13 14:00:46 +1000

    adt: replace adt_get_child_count() with Rust
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- rust/src/adt.rs
+++ rust/src/adt.rs
@@ -268,6 +268,12 @@ pub unsafe extern "C" fn adt_first_child_offset(_dt: *const c_void, offset: c_in
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn adt_get_child_count(_dt: *const c_void, offset: c_int) -> c_int {
+    let ptr: *const ADTNode = unsafe { adt.add(offset as usize) as *const ADTNode };
+    ADTNode::from_ptr(ptr).unwrap().child_count as c_int
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn adt_get_property_by_offset(
     _dt: *const c_void,
     offset: c_int,
