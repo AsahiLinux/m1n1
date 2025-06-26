ad36e16ffe96fefed60c24791b23bbcc27f457aa
James Calligeros <jcalligeros99@gmail.com>
2025-06-26 21:54:47 +1000

    adt: replace adt_get_reg() with Rust
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- rust/src/adt.rs
+++ rust/src/adt.rs
@@ -367,6 +367,157 @@ impl<'a> ADT<'a> {
     pub fn path_offset(path: &str) -> Result<i32, AdtError> {
         ADT::path_offset_trace(path, None)
     }
+
+    fn get_cells(src: &[u32]) -> u64 {
+        let mut val: u64 = 0;
+
+        src.iter()
+            .enumerate()
+            .for_each(|(i, &x)| val |= (x as u64) << (32 * i));
+
+        val
+    }
+
+    /// Retrieve the register container (addr, size) of a given node. Each addr
+    /// and size is a contiguous array of u32s with `#address-cells` or `#size-cells`
+    /// elements. The entire `reg` property is similarly a contiguous array of these
+    /// containers [(addr, size), (addr, size), ...]. The `i` argument is an index
+    /// into this array.
+    pub fn get_reg_container(offsets: &[i32], pname: &str, i: i32) -> Result<(u64, u64), AdtError> {
+        let mut cursor: usize = offsets.len() - 1;
+
+        if offsets[0] == 0 {
+            return Err(AdtError::BadOffset);
+        }
+
+        let mut node = offsets[cursor];
+        let mut parent = {
+            if cursor > 0 {
+                offsets[cursor - 1]
+            } else {
+                0
+            }
+        };
+
+        let mut addr_cells = u32::from_ne_bytes(
+            ADT::get_property_by_name(parent, "#address-cells")
+                .unwrap()
+                .value[..4]
+                .try_into()
+                .unwrap(),
+        );
+
+        let mut size_cells = u32::from_ne_bytes(
+            ADT::get_property_by_name(parent, "#size-cells")
+                .unwrap()
+                .value[..4]
+                .try_into()
+                .unwrap(),
+        );
+
+        if addr_cells < 1 || addr_cells > 2 || size_cells > 2 {
+            return Err(AdtError::BadNCells);
+        }
+
+        let reg = ADT::get_property_by_name(node, pname)?;
+
+        if reg.size == 0 {
+            return Err(AdtError::NotFound);
+        }
+
+        // Check that the reg property is big enough to contain a reg
+        // container at index i.
+        if reg.size < (i as u32 + 1) * (addr_cells + size_cells) * 4 {
+            return Err(AdtError::BadValue);
+        }
+
+        let addr_elems: &[u32];
+        let size_elems: &[u32];
+
+        // SAFETY: For a reg property, this pointer arithmetic is always true,
+        // as we check above that i is within bounds.
+        unsafe {
+            let aref_ptr: *const u32 = (reg.value.as_ptr() as *const u32)
+                .add((i as u32 * (addr_cells + size_cells)) as usize);
+
+            let sref_ptr = aref_ptr.add((addr_cells) as usize);
+
+            addr_elems = core::slice::from_raw_parts(aref_ptr, addr_cells as usize);
+
+            size_elems = core::slice::from_raw_parts(sref_ptr, size_cells as usize);
+        }
+
+        let mut addr = ADT::get_cells(addr_elems);
+        let size = ADT::get_cells(size_elems);
+
+        while parent != 0 {
+            cursor -= 1;
+            node = parent;
+            parent = {
+                if cursor > 0 {
+                    offsets[cursor - 1]
+                } else {
+                    0
+                }
+            };
+
+            let ranges = ADT::get_property_by_name(node, "ranges")?;
+
+            let paddr_cells = u32::from_ne_bytes(
+                ADT::get_property_by_name(parent, "#address-cells")
+                    .unwrap()
+                    .value[..4]
+                    .try_into()
+                    .unwrap(),
+            );
+
+            if paddr_cells < 1 || paddr_cells > 2 || size_cells > 2 {
+                return Err(AdtError::BadNCells);
+            }
+
+            let mut n_ranges = ranges.size / (4 * (paddr_cells + addr_cells + size_cells));
+
+            while n_ranges != 0 {
+                let ca_ref: &[u32];
+                let pa_ref: &[u32];
+                let cs_ref: &[u32];
+
+                unsafe {
+                    let ca_ptr = ranges.value.as_ptr() as *const u32;
+                    let pa_ptr = ca_ptr.add(addr_cells as usize);
+                    let cs_ptr = pa_ptr.add(addr_cells as usize);
+
+                    ca_ref = core::slice::from_raw_parts(ca_ptr, addr_cells as usize);
+
+                    pa_ref = core::slice::from_raw_parts(pa_ptr, paddr_cells as usize);
+
+                    cs_ref = core::slice::from_raw_parts(cs_ptr, size_cells as usize);
+                }
+
+                let child_addr = ADT::get_cells(ca_ref);
+                let parent_addr = ADT::get_cells(pa_ref);
+                let child_size = ADT::get_cells(cs_ref);
+
+                if addr >= child_addr && (addr + size) <= (child_addr + child_size) {
+                    addr = addr - child_addr + parent_addr;
+                    break;
+                }
+                n_ranges -= 1;
+            }
+
+            size_cells = u32::from_ne_bytes(
+                ADT::get_property_by_name(parent, "#size-cells")
+                    .unwrap()
+                    .value[..4]
+                    .try_into()
+                    .unwrap(),
+            );
+
+            addr_cells = paddr_cells;
+        }
+
+        Ok((addr, size))
+    }
 }
 
 extern "C" {
@@ -606,5 +757,54 @@ pub unsafe extern "C" fn adt_path_offset(_dt: *const c_void, path: *const c_char
     match ADT::path_offset(strpath) {
         Ok(offset) => offset as c_int,
         Err(e) => e as c_int,
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn adt_get_reg(
+    _dt: *const c_void,
+    offsets: *mut i32,
+    prop: *const c_char,
+    i: c_int,
+    paddr: *mut u64,
+    psize: *mut u64,
+) -> c_int {
+    let strname: &str = unsafe { CStr::from_ptr(prop).to_str().unwrap() };
+    let o: &[i32];
+
+    unsafe {
+        // Unlike adt_path_offset_trace(), we need to know
+        // the exact number of nodes in the path.
+        let mut n_offs: usize = 0;
+        if *offsets == 0 {
+            return AdtError::BadOffset as c_int;
+        }
+
+        while *offsets.add(n_offs) != 0 {
+            n_offs += 1;
+        }
+
+        o = core::slice::from_raw_parts(offsets, n_offs);
+    }
+
+    match ADT::get_reg_container(o, strname, i) {
+        Ok((a, s)) => {
+            if !paddr.is_null() {
+                unsafe {
+                    *paddr = a;
+                }
+            }
+
+            if !psize.is_null() {
+                unsafe {
+                    *psize = s;
+                }
+            }
+
+            return 0;
+        }
+        Err(e) => {
+            return e as c_int;
+        }
     }
 }
