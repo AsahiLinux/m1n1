f674c13945a72ccc9304f32b471574b76a393d7d
Yureka <yureka@cyberchaos.dev>
2026-01-24 09:55:43 +0100

    smp: add CPU_START_OFF for T8132
    Signed-off-by: Yureka <yureka@cyberchaos.dev>

--- src/smp.c
+++ src/smp.c
@@ -267,6 +267,7 @@ void smp_start_secondaries(void)
             break;
         case T8112:
         case T8122:
+        case T8132:
             cpu_start_off = CPU_START_OFF_T8112;
             break;
         case T6020:
