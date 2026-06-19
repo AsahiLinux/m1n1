f7566d481bf83b4037e4d9b0f3fd28724621b4c8
Janne Grunau <j@jannau.net>
2026-06-19 09:20:53 +0200

    chickens: everest: Move SYS_IMP_APL_HID4 to common
    Signed-off-by: Janne Grunau <j@jannau.net>

--- src/chickens_everest.c
+++ src/chickens_everest.c
@@ -21,6 +21,8 @@ static void init_common_everest(void)
                  BIT(4) | BIT(0));
 
     reg_clr(s3_0_c15_c2_4, BIT(0) | BIT(1) | BIT(16) | BIT(17) | BIT(18) | BIT(22));
+
+    reg_set(SYS_IMP_APL_HID4, HID4_ENABLE_LFSR_STALL_LOAD_PIPE2_ISSUE);
 }
 
 void init_t8122_everest(int rev)
@@ -47,8 +49,6 @@ void init_t8122_everest(int rev)
                             (0x2 << 24) | (0x2 << 28) | (0x2uL << 32)) |
             HID26_GROUP2_OFFSET(0x23 | (0x1 << 8) | (0x1 << 12) | (0x1 << 16) | (0x1 << 20) |
                                 (0x1 << 24)));
-
-    reg_set(SYS_IMP_APL_HID4, HID4_ENABLE_LFSR_STALL_LOAD_PIPE2_ISSUE);
 }
 
 void init_t6030_everest(int rev)
@@ -80,8 +80,6 @@ void init_t6030_everest(int rev)
     reg_set(SYS_IMP_APL_HID18, BIT(61));
 
     reg_set(s3_0_c15_c2_3, BIT(3));
-
-    reg_set(SYS_IMP_APL_HID4, HID4_ENABLE_LFSR_STALL_LOAD_PIPE2_ISSUE);
 }
 
 void init_t6031_everest(int rev)
@@ -107,6 +105,4 @@ void init_t6031_everest(int rev)
     msr(SYS_IMP_APL_HID26, HID26_GROUP1_OFFSET(0xF88F65588LL) | HID26_GROUP2_OFFSET(0x3F28));
     /* This is new to M3 and i have no idea what it is yet */
     reg_set(s3_0_c15_c2_3, BIT(3));
-
-    reg_set(SYS_IMP_APL_HID4, HID4_ENABLE_LFSR_STALL_LOAD_PIPE2_ISSUE);
 }
